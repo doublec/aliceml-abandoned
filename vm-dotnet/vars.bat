f7ab9d64310dac55e2c9905c0d11f0bc348e911c
kornstae <none@none>
1999-07-26 23:40:37 +0100

    Preliminary version of the runtime.

---
+++ vm-dotnet/vars.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+SET PATH=C:\CorSDK\bin;C:\CorSDK\samples;C:\CorSDK\Compiler\VC;%PATH%
+SET INCLUDE=C:\Windows\System;D:\StockWerk;
+SET LIB=C:\CorSDK\Compiler\VC;C:\Programme\DevStudio\VC\lib;D:\StockWerk;C:\Windows\System;
+
+SET CORPATH=D:\StockWerk;C:\Windows\System;
