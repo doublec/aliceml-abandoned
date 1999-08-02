256a1d5646828831dc0435d9f2a18c8c13dce798
kornstae <none@none>
1999-08-02 14:10:36 +0100

    It's useful to have cygwin in the path (though not necessary).

--- vm-com+/vars.bat
+++ vm-com+/vars.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 
+SET PATH=C:\Programme\Cygnus\cygwin-b20\H-i586-cygwin32\bin;%PATH%
 SET PATH=C:\CorSDK\bin;C:\CorSDK\samples;C:\CorSDK\Compiler\VC;%PATH%
 SET INCLUDE=C:\Windows\System;D:\StockWerk;
 SET LIB=C:\CorSDK\Compiler\VC;C:\Programme\DevStudio\VC\lib;D:\StockWerk;C:\Windows\System;
