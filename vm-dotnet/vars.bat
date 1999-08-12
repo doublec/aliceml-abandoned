95455e093e9a374b5d1f2d3648aee8fdec79ddc5
kornstae <none@none>
1999-08-12 10:21:33 +0100

    SML-enabled.

--- vm-dotnet/vars.bat
+++ vm-dotnet/vars.bat
@@ -1,8 +1,12 @@
 @ECHO OFF
 
 SET PATH=C:\Programme\Cygnus\cygwin-b20\H-i586-cygwin32\bin;%PATH%
+SET PATH=C:\Programme\GNU\WinCvs;%PATH%
 SET PATH=C:\CorSDK\bin;C:\CorSDK\samples;C:\CorSDK\Compiler\VC;%PATH%
 SET INCLUDE=C:\Windows\System;D:\StockWerk;
 SET LIB=C:\CorSDK\Compiler\VC;C:\Programme\DevStudio\VC\lib;D:\StockWerk;C:\Windows\System;
 
 SET CORPATH=D:\StockWerk;C:\Windows\System;
+
+SET PATH=C:\Programme\smlnj\bin;%PATH%
+SET CM_PATH=C:\Programme\smlnj\lib
