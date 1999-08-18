070ec6ecb5da250fbc18a41f63bf0875a9471b52
kornstae <none@none>
1999-08-18 22:40:34 +0100

    Works on kitten and heaven now.

--- vm-com+/vars.bat
+++ vm-com+/vars.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
-SET PATH=C:\Programme\Cygnus\cygwin-b20\H-i586-cygwin32\bin;%PATH%
-SET PATH=C:\Programme\GNU\WinCvs;%PATH%
+SET PATH=C:\Progra~1\Cygnus\cygwin-b20\H-i586-cygwin32\bin;%PATH%
+SET PATH=C:\Progra~1\GNU\WinCvs;%PATH%
 SET PATH=C:\CorSDK\bin;C:\CorSDK\samples;C:\CorSDK\Compiler\VC;%PATH%
-SET INCLUDE=C:\Windows\System;D:\StockWerk;
-SET LIB=C:\CorSDK\Compiler\VC;C:\Programme\DevStudio\VC\lib;D:\StockWerk;C:\Windows\System;
+SET INCLUDE=C:\Windows\System;
+SET LIB=C:\CorSDK\Compiler\VC;C:\Programme\DevStudio\VC\lib;C:\Windows\System;
 
-SET CORPATH=D:\StockWerk;C:\Windows\System;
+SET CORPATH=C:\Windows\System;
 
-SET PATH=C:\Programme\smlnj\bin;%PATH%
-SET CM_PATH=C:\Programme\smlnj\lib
+SET PATH=C:\Progra~1\smlnj\bin;%PATH%
+SET CM_PATH=C:\Progra~1\smlnj\lib
