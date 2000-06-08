ae2a44f1779e5ab8a8abf100e2336539c5c24521
kornstae <none@none>
2000-06-08 20:35:29 +0100

    Missing bits for the distribution.

--- vm-dotnet/vars.bat
+++ vm-dotnet/vars.bat
@@ -1,8 +1,7 @@
 @ECHO OFF
 
-SET PATH=C:\Progra~1\Cygnus\cygwin-b20\H-i586-cygwin32\bin;%PATH%
+SET PATH=C:\Progra~1\cygwin-b20\H-i586-cygwin32\bin;%PATH%
 SET PATH=C:\Progra~1\GNU\WinCvs;%PATH%
-SET PATH=C:\CorSDK\bin;C:\CorSDK\samples;C:\CorSDK\Compiler\VC;%PATH%
 SET INCLUDE=C:\Windows\System;
 SET LIB=C:\CorSDK\Compiler\VC;C:\Programme\DevStudio\VC\lib;C:\Windows\System;
 
