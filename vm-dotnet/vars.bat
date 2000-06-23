27991bfb1f3a553bb9157bc084ab88532bb4c1c1
kornstae <none@none>
2000-06-23 10:55:18 +0100

    Added some missing files; Cleanups.

--- vm-dotnet/vars.bat
+++ vm-dotnet/vars.bat
@@ -1,13 +1,5 @@
 @ECHO OFF
 
-SET PATH=C:\Progra~1\cygwin-b20\H-i586-cygwin32\bin;%PATH%
-SET PATH=C:\Progra~1\GNU\WinCvs;%PATH%
-SET INCLUDE=C:\Windows\System;
-SET LIB=C:\CorSDK\Compiler\VC;C:\Programme\DevStudio\VC\lib;C:\Windows\System;
-
-SET CORPATH=..\..\vm-com+;C:\Windows\System;
-
-SET PATH=C:\Progra~1\smlnj\bin;%PATH%
-SET CM_PATH=C:\Progra~1\smlnj\lib
-
-doskey
+SET STOCKHOME=C:\Alice
+SET PATH=%STOCKHOME%\SML\bin;%PATH%
+SET CORPATH=%STOCKHOME%
