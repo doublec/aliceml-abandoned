1e22c2a66a3cf33fc86cea9dd88b497b3c48eab4
bruni <none@none>
2000-07-12 17:11:12 +0100

    Added header and minor buf fixes

--- store/headerdef.hh
+++ store/headerdef.hh
@@ -1,3 +1,14 @@
+//
+// Author:
+//   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
+//
+// Copyright:
+//   Thorsten Brunklaus, 2000
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
 #ifndef __HEADERDEF_HH__
 #define __HEADERDEF_HH__
 
