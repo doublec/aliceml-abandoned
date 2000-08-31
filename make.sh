835b15d3a93f1b232cb8ca0f218caeae4df78f6e
rossberg <none@none>
2000-08-31 15:33:05 +0100

    More automation.

--- make.sh
+++ make.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
-# Build all files needed to bootstrap the Alice-to-Mozart compiler.
+# Bootstrap the Alice-to-Mozart compiler.
+
+# To install globally:
+#   prefix=/opt/stockhausen-devel
+# To install locally:
+#   prefix=`pwd`/install
+
+if [ "$1" = "-global" ]
+then
+   prefix=/opt/stockhausen-devel
+else
+   prefix=`pwd`/install
+fi
+
+echo Trying to install Stockhausen to $prefix...
 
 rm -f stoc/stoc-mozart.*
 (cd stoc && make) || exit 1
 (cd vm-mozart && make) || exit 1
-(cd vm-mozart/bootstrap && make) || exit 1
-# (cd vm-mozart/boostrap && make install clean bootstrap install)
+(cd vm-mozart/bootstrap && make PREFIX=$prefix all) || exit 1
+(cd vm-mozart/bootstrap && make PREFIX=$prefix install) || exit 1
+(cd vm-mozart/bootstrap && make PREFIX=$prefix clean) || exit 1
+(cd vm-mozart/bootstrap && make PREFIX=$prefix bootstrap) || exit 1
+(cd vm-mozart/bootstrap && make PREFIX=$prefix install) || exit 1
