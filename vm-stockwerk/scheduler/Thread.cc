fb818b489cfbee2011924ddac598fc99fa6d8859
kornstae <none@none>
2000-09-14 22:07:17 +0100

    Auxiliary classes compile.

---
+++ vm-stockwerk/scheduler/Thread.cc
@@ -0,0 +1,13 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2000
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#include "Thread.hh"
