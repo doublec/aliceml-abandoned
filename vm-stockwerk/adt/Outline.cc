20ed68d174fc0be15651f4d1d71dcfb29d005480
bruni <none@none>
2000-09-28 14:16:33 +0100

    Added Debug Helper

---
+++ vm-stockwerk/adt/Outline.cc
@@ -0,0 +1,14 @@
+//
+// Author:
+//   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
+//
+// Copyright:
+//   Thorsten Brunklaus, 2000
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+#if defined(INTERFACE)
+#pragma implementation "Stack.hh"
+#endif
