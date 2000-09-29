abf225d17f3bcaef0073b89276b068a7dd3b9146
kornstae <none@none>
2000-09-29 14:19:49 +0100

    Implemented.

--- adt/Queue.hh
+++ adt/Queue.hh
@@ -19,16 +19,70 @@
 
 #include "store/store.hh"
 
+#define QueueLabel Store::MakeLabel(0) //--**
+#define QueueArrayLabel Store::MakeLabel(0) //--**
+
 class Queue: private Block {
+private:
+  static const u_int SIZE = 3;
+  static const u_int WRITE_INDEX_POS = 1;
+  static const u_int READ_INDEX_POS = 2;
+  static const u_int ARRAY_POS = 3;
+
+  void Enlarge() {
+    Block *oldArray = Store::WordToBlock(GetArg(ARRAY_POS));
+    u_int oldSize = oldArray->GetSize();
+    u_int newSize = oldSize * 3 / 2;
+    Block *newArray = Store::AllocBlock(QueueArrayLabel, newSize);
+    u_int index = Store::WordToInt(GetArg(READ_INDEX_POS));
+    Assert(index == Store::WordToInt(GetArg(WRITE_INDEX_POS)));
+    word *oldBase = oldArray->GetBase();
+    word *newBase = newArray->GetBase();
+    std::memcpy(newBase, oldBase + index, oldSize - index);
+    std::memcpy(newBase + index, oldBase, index);
+    ReplaceArg(WRITE_INDEX_POS, Store::IntToWord(index));
+    ReplaceArg(READ_INDEX_POS, Store::IntToWord(0));
+    ReplaceArg(ARRAY_POS, newArray->ToWord());
+  }
 public:
   using Block::ToWord;
 
-  static Queue *New(); //--** implement
-  static Queue *FromWord(word w); //--** implement
+  static Queue *New(u_int initialSize) {
+    Block *b = Store::AllocBlock(QueueLabel, SIZE);
+    Block *array = Store::AllocBlock(QueueArrayLabel, initialSize);
+    b->InitArg(WRITE_INDEX_POS, Store::IntToWord(0));
+    b->InitArg(READ_INDEX_POS, Store::IntToWord(0));
+    b->InitArg(ARRAY_POS, array->ToWord());
+    return static_cast<Queue *>(b);
+  }
+  static Queue *FromWord(word x) {
+    Block *b = Store::WordToBlock(x);
+    Assert(b == INVALID_POINTER || b->GetLabel() == QueueLabel);
+    return static_cast<Queue *>(b);
+  }
 
-  void Enqueue(word w); //--** implement
-  bool IsEmpty(); //--** implement
-  word Dequeue(); //--** implement
+  void Enqueue(word w) {
+    u_int writeIndex = Store::WordToInt(GetArg(WRITE_INDEX_POS));
+    u_int readIndex = Store::WordToInt(GetArg(READ_INDEX_POS));
+    Block *array = Store::WordToBlock(GetArg(ARRAY_POS));
+    array->ReplaceArg(writeIndex + 1, w);
+    u_int size = array->GetSize();
+    writeIndex = (writeIndex + 1) % size;
+    if (writeIndex == readIndex)
+      Enlarge();
+  }
+  bool IsEmpty() {
+    return GetArg(READ_INDEX_POS) == GetArg(WRITE_INDEX_POS);
+  }
+  word Dequeue() {
+    u_int readIndex = Store::WordToInt(GetArg(READ_INDEX_POS));
+    Block *array = Store::WordToBlock(GetArg(ARRAY_POS));
+    Assert(readIndex != Store::WordToInt(GetArg(WRITE_INDEX_POS)));
+    readIndex++;
+    word result = array->GetArg(readIndex);
+    ReplaceArg(READ_INDEX_POS, Store::IntToWord(readIndex % array->GetSize()));
+    return result;
+  }
 };
 
 #endif __ADT__QUEUE_HH__
