529ac05db681143ac2d9fe53817db5a557530d2d
kornstae <none@none>
2000-09-29 14:59:03 +0100

    Added member function Blank.

--- adt/Queue.hh
+++ adt/Queue.hh
@@ -29,10 +29,10 @@ private:
   static const u_int READ_INDEX_POS = 2;
   static const u_int ARRAY_POS = 3;
 
-  void Enlarge() {
+  void Enlarge(u_int threshold) {
     Block *oldArray = Store::WordToBlock(GetArg(ARRAY_POS));
     u_int oldSize = oldArray->GetSize();
-    u_int newSize = oldSize * 3 / 2;
+    u_int newSize = oldSize + threshold;
     Block *newArray = Store::AllocBlock(QueueArrayLabel, newSize);
     u_int index = Store::WordToInt(GetArg(READ_INDEX_POS));
     Assert(index == Store::WordToInt(GetArg(WRITE_INDEX_POS)));
@@ -69,7 +69,7 @@ public:
     u_int size = array->GetSize();
     writeIndex = (writeIndex + 1) % size;
     if (writeIndex == readIndex)
-      Enlarge();
+      Enlarge(size / 2);
   }
   bool IsEmpty() {
     return GetArg(READ_INDEX_POS) == GetArg(WRITE_INDEX_POS);
@@ -82,6 +82,38 @@ public:
     word result = array->GetArg(readIndex);
     ReplaceArg(READ_INDEX_POS, Store::IntToWord(readIndex % array->GetSize()));
     return result;
+  }
+
+  void Blank(u_int threshold) {
+    Block *array = Store::WordToBlock(GetArg(ARRAY_POS));
+    u_int readIndex = Store::WordToInt(GetArg(READ_INDEX_POS));
+    u_int writeIndex = Store::WordToInt(GetArg(READ_INDEX_POS));
+    u_int oldSize = array->GetSize();
+    u_int newSize = (writeIndex + size - readIndex) % size + threshold;
+    if (newSize < oldSize) {
+      word *base = array->GetBase();
+      if (readIndex < writeIndex) {
+	u_int length = writeIndex - readIndex;
+	std::memmove(base, base + readIndex, length);
+	ReplaceArg(READ_INDEX_POS, Store::IntToWord(0));
+	ReplaceArg(WRITE_INDEX_POS, Store::IntToWord(length));
+      } else {
+	u_int length = oldSize - readIndex;
+	u_int newReadIndex = newSize - length;
+	std::memmove(base + newReadIndex, base + readIndex, length);
+	ReplaceArg(READ_INDEX_POS, Store::IntToWord(newReadIndex));
+      }
+      HeaderOp::EncodeSize(array, newSize);
+    }
+    if (readIndex < writeIndex) {
+      for (u_int i = writeIndex + 1; i <= newSize; i++)
+	array->ReplaceArg(i, Store::IntToWord(0));
+      for (u_int i = 1; i <= readIndex; i++)
+	array->ReplaceArg(i, Store::IntToWord(0));
+    } else {
+      for (u_int i = writeIndex + 1; i <= readIndex; i++)
+	array->ReplaceArg(i, Store::IntToWord(0));
+    }
   }
 };
 
