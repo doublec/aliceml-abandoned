54ce93784573b520d4b80b8444920a8034c7c102
bruni <none@none>
2000-10-06 09:52:57 +0100

    some cleanups

--- vm-stockwerk/store/Handler.hh
+++ vm-stockwerk/store/Handler.hh
@@ -1,3 +1,16 @@
+//
+// Authors:
+//   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Thorsten Brunklaus, 2000
+//   Leif Kornstaedt, 2000
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
 #ifndef __STORE__HANDLER_HH__
 #define __STORE__HANDLER_HH__
 
