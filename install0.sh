dd60bdaaff462becd5a53dbe732e1591782c4443
rossberg <none@none>
2000-12-07 15:32:16 +0000

    * Don't rm. * Use install instead of cp/chmod.

--- install0.sh
+++ install0.sh
@@ -8,23 +8,21 @@ else
 fi
 
 # Cleanup/Create Directories
-rm -rf $PREFIX/bin $PREFIX/lib
-mkdir $PREFIX/bin
-mkdir $PREFIX/lib
+#rm -rf $PREFIX/bin $PREFIX/lib
+mkdir $PREFIX/bin 2>/dev/null
+mkdir $PREFIX/lib 2>/dev/null
 # Copy Main Files
-cp stoc/backend-mozart/stoc-mozart.exe $PREFIX/
-cp stoc/stoc-mozart.x86-linux $PREFIX/
+install -c -m444 stoc/backend-mozart/stoc-mozart.exe $PREFIX/
+install -c -m444 stoc/stoc-mozart.x86-linux $PREFIX/
 # Copy System Libraries
-cp vm-mozart/*.ozf $PREFIX/lib
-cp vm-mozart/bootstrap/lib/*.ozf.sig $PREFIX/lib/
+install -c -m444 vm-mozart/*.ozf $PREFIX/lib
+install -c -m444 vm-mozart/bootstrap/lib/*.ozf.sig $PREFIX/lib/
 #(cd vm-mozart/bootstrap && make Default.import) || exit 1
-cp vm-mozart/bootstrap/Base.import $PREFIX/Default.import
+install -c -m444 vm-mozart/bootstrap/Base.import $PREFIX/Default.import
 # Copy Base Lib
 (cd vm-mozart/bootstrap && rm -f lib/Base.ozf && make lib/Base.ozf) || exit 1
-cp vm-mozart/bootstrap/lib/Base.ozf $PREFIX/lib
-cp vm-com+/Base.dll.sig $PREFIX/lib/Base.ozf.sig
+install -c -m444 vm-mozart/bootstrap/lib/Base.ozf $PREFIX/lib
+install -c -m444 vm-com+/Base.dll.sig $PREFIX/lib/Base.ozf.sig
 # Copy Bin Files
-cp vm-mozart/bootstrap/stoc.test $PREFIX/bin/stoc
-chmod +x $PREFIX/bin/stoc
-cp vm-mozart/bootstrap/stow.test $PREFIX/bin/stow
-chmod +x $PREFIX/bin/stow
+install -c -m555 vm-mozart/bootstrap/stoc.test $PREFIX/bin/stoc
+install -c -m555 vm-mozart/bootstrap/stow.test $PREFIX/bin/stow
