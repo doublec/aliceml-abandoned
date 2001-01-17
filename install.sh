d2a39f85702b7f5f99e18d41f673ec05506bdab1
kornstae <none@none>
2001-01-17 18:29:55 +0000

    Renamed install.sh to install0.sh and make.sh to install.sh.

--- install.sh
+++ install.sh
@@ -1,67 +1,25 @@
 #!/bin/sh
 
-# Install the non-bootstrapped system, used for testing the libraries.
+# Bootstrap the Alice-to-Mozart compiler.
 
-if [$1 = ""]
+# To install globally:
+#   prefix=/opt/stockhausen-devel
+# To install locally:
+#   prefix=`pwd`/install
+
+if [ "$1" = "-global" ]
 then
-  PREFIX=/opt/stockhausen-devel0
+   prefix=/opt/stockhausen-devel
 else
-  PREFIX=$1
+   prefix=`pwd`/install
 fi
+
+echo Trying to install Stockhausen to $prefix...
 
 PLATFORM=x86-linux
 
-#(cd bootstrap &&
-# rm -f stoc-mozart.$PLATFORM &&
-# make stoc-mozart.exe stoc-mozart.$PLATFORM) || exit 1
-
-LIBS="
-	build1/lib/fundamental/Fundamental.ozf
-	build1/lib/system/IO.asig
-	build1/lib/system/IO.ozf
-	build1/lib/system/TextIO.asig
-	build1/lib/system/TextIO.ozf
-	build1/lib/system/OS.asig
-	build1/lib/system/OS.ozf
-	build1/lib/system/Unix.asig
-	build1/lib/system/Unix.ozf
-	build1/lib/system/CommandLine.asig
-	build1/lib/system/CommandLine.ozf
-	build1/lib/system/Tools.asig
-	build1/lib/system/Tools.ozf
-	build1/lib/system/UnsafePickle.asig
-	build1/lib/system/UnsafePickle.ozf
-	build1/lib/system/Debug.asig
-	build1/lib/system/Debug.ozf
-	build1/lib/system/Socket.asig
-	build1/lib/system/Socket.ozf
-"
-
-(cd vm-mozart/bootstrap &&
- make depend $LIBS) || exit 1
-
-ozl vm-mozart/bootstrap/build1/lib/fundamental/Fundamental.ozf \
-    -o vm-mozart/bootstrap/build1/lib/fundamental/LinkedFundamental.ozf
-
-# Create Directories
-mkdir $PREFIX 2>/dev/null
-mkdir $PREFIX/bin 2>/dev/null
-mkdir $PREFIX/lib 2>/dev/null
-mkdir $PREFIX/lib/fundamental 2>/dev/null
-mkdir $PREFIX/lib/system 2>/dev/null
-# Copy Top Files
-install -c -m444 bootstrap/stoc-mozart.exe $PREFIX/
-install -c -m444 bootstrap/stoc-mozart.x86-linux $PREFIX/
-# Copy Bin Files
-install -c -m555 vm-mozart/bootstrap/stoc.test $PREFIX/bin/stoc
-install -c -m555 vm-mozart/bootstrap/stow.test $PREFIX/bin/stow
-# Copy System Libraries
-install -c -m444 lib/fundamental/Fundamental.import $PREFIX/Default.import
-install -c -m444 \
-	vm-mozart/bootstrap/build1/lib/fundamental/LinkedFundamental.ozf \
-	$PREFIX/lib/fundamental/Fundamental.ozf
-install -c -m444 vm-com+/Base.asig $PREFIX/lib/fundamental/Fundamental.asig
-install -c -m444 vm-mozart/bootstrap/build1/lib/system/*.ozf \
-	$PREFIX/lib/system
-install -c -m444 vm-mozart/bootstrap/build1/lib/system/*.asig \
-	$PREFIX/lib/system
+rm -f bootstrap/stoc-mozart.$PLATFORM bootstrap/stodep.$PLATFORM
+(cd bootstrap && make) || exit 1
+(cd vm-mozart/bootstrap && make depend) || exit 1
+(cd vm-mozart/bootstrap && make build3-install) || exit 1
+(cd vm-mozart/bootstrap && make PREFIX=$prefix install) || exit 1
