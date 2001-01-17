d9610868bd9254ca5b00de06f58ce20603ba9114
kornstae <none@none>
2001-01-17 18:28:02 +0000

    Works again.  Whether I would recommend to use it is another story though. ;-)

--- install0.sh
+++ install0.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 # Install the non-bootstrapped system, used for testing the libraries.
-# This has to be preceded by the following commands from make.sh:
-#    rm -f stoc/stoc-mozart.*
-#    (cd stoc && make) || exit 1
-#    (cd vm-mozart && make) || exit 1
-
 
 if [$1 = ""]
 then
@@ -14,23 +9,59 @@ else
   PREFIX=$1
 fi
 
-# Copy Base Lib
-(cd vm-mozart/bootstrap && rm -f lib/Base.ozf && make first lib/Base.ozf) || exit 1
+PLATFORM=x86-linux
 
-# Cleanup/Create Directories
-#rm -rf $PREFIX/bin $PREFIX/lib
+#(cd bootstrap &&
+# rm -f stoc-mozart.$PLATFORM &&
+# make stoc-mozart.exe stoc-mozart.$PLATFORM) || exit 1
+
+LIBS="
+	build1/lib/fundamental/Fundamental.ozf
+	build1/lib/system/IO.asig
+	build1/lib/system/IO.ozf
+	build1/lib/system/TextIO.asig
+	build1/lib/system/TextIO.ozf
+	build1/lib/system/OS.asig
+	build1/lib/system/OS.ozf
+	build1/lib/system/Unix.asig
+	build1/lib/system/Unix.ozf
+	build1/lib/system/CommandLine.asig
+	build1/lib/system/CommandLine.ozf
+	build1/lib/system/Tools.asig
+	build1/lib/system/Tools.ozf
+	build1/lib/system/UnsafePickle.asig
+	build1/lib/system/UnsafePickle.ozf
+	build1/lib/system/Debug.asig
+	build1/lib/system/Debug.ozf
+	build1/lib/system/Socket.asig
+	build1/lib/system/Socket.ozf
+"
+
+(cd vm-mozart/bootstrap &&
+ make depend $LIBS) || exit 1
+
+ozl vm-mozart/bootstrap/build1/lib/fundamental/Fundamental.ozf \
+    -o vm-mozart/bootstrap/build1/lib/fundamental/LinkedFundamental.ozf
+
+# Create Directories
+mkdir $PREFIX 2>/dev/null
 mkdir $PREFIX/bin 2>/dev/null
 mkdir $PREFIX/lib 2>/dev/null
-# Copy Main Files
-install -c -m444 stoc/backend-mozart/stoc-mozart.exe $PREFIX/
-install -c -m444 stoc/stoc-mozart.x86-linux $PREFIX/
-# Copy System Libraries
-install -c -m444 vm-mozart/*.ozf $PREFIX/lib
-install -c -m444 vm-mozart/bootstrap/lib/*.ozf.sig $PREFIX/lib/
-#(cd vm-mozart/bootstrap && make Default.import) || exit 1
-install -c -m444 vm-mozart/bootstrap/Base.import $PREFIX/Default.import
-install -c -m444 vm-mozart/bootstrap/lib/Base.ozf $PREFIX/lib
-install -c -m444 vm-com+/Base.dll.sig $PREFIX/lib/Base.ozf.sig
+mkdir $PREFIX/lib/fundamental 2>/dev/null
+mkdir $PREFIX/lib/system 2>/dev/null
+# Copy Top Files
+install -c -m444 bootstrap/stoc-mozart.exe $PREFIX/
+install -c -m444 bootstrap/stoc-mozart.x86-linux $PREFIX/
 # Copy Bin Files
 install -c -m555 vm-mozart/bootstrap/stoc.test $PREFIX/bin/stoc
 install -c -m555 vm-mozart/bootstrap/stow.test $PREFIX/bin/stow
+# Copy System Libraries
+install -c -m444 lib/fundamental/Fundamental.import $PREFIX/Default.import
+install -c -m444 \
+	vm-mozart/bootstrap/build1/lib/fundamental/LinkedFundamental.ozf \
+	$PREFIX/lib/fundamental/Fundamental.ozf
+install -c -m444 vm-com+/Base.asig $PREFIX/lib/fundamental/Fundamental.asig
+install -c -m444 vm-mozart/bootstrap/build1/lib/system/*.ozf \
+	$PREFIX/lib/system
+install -c -m444 vm-mozart/bootstrap/build1/lib/system/*.asig \
+	$PREFIX/lib/system
