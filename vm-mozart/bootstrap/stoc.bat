2e3646291f9d5577cf65f630f0a418862050f688
kornstae <none@none>
2001-01-18 10:15:06 +0000

    Removed the vm-mozart/bootstrap directory; Moved "native" implementations of system components to vm-mozart/lib/system.

--- vm-mozart/bootstrap/stoc.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set STOCKHOME=D:\Program Files\stockhausen
-
-stow x-alice:/stoc.ozf %1 %2 %3 %4 %5 %6 %7 %8 %9
--- vm-mozart/bootstrap/stow.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set OZ_LOAD=prefix=x-alice:/=D:/Program Files/stockhausen/;cache=D:/Program Files/Mozart/cache
-
-set PATH=D:\Program Files\Mozart\bin\;%PATH%
-
-ozengine x-alice:/stow.ozf %1 %2 %3 %4 %5 %6 %7 %8 %9
