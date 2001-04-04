3d845b58e3e20d0059983d57d61fd54043241649
kornstae <none@none>
2001-04-04 17:11:36 +0100

    Renamed to vm-dotnet.

--- vm-com+/examples/Test.cs
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void Main(string[] args) {
-	System.Console.WriteLine(Fib_Alice.Lang);
-	int[] array = Fib_Alice.NextTerms(5, 1, 1);
-	for (int i = 0; i < 5; i++)
-	    System.Console.WriteLine(array[i]);
-    }
-}
--- vm-com+/stoc.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"%STOCKHOME%\SML\sml.bat" @SMLload="%STOCKHOME%\stoc-dotnet" %1 %2 %3 %4 %5 %6 %7 %8 %9
