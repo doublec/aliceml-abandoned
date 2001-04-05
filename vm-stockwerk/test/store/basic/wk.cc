9dfc2699655ba70e348e07636d8d19dbcdb0f5d5
bruni <none@none>
2001-04-05 13:50:37 +0100

    Adapted wk test

--- vm-stockwerk/test/store/basic/wk.cc
+++ vm-stockwerk/test/store/basic/wk.cc
@@ -37,13 +37,37 @@ int main(void) {
   for (u_int i = 0; i < 20; i++) {
     dict->InsertItem(i, Store::IntToWord(1000 * (i + 1)));
   }
+  Block *p = Store::AllocBlock(Store::MakeLabel(0), 2048);
+  dict->InsertItem(20, p->ToWord());
   std::printf("done\n");
   std::printf("Accessing Items...\n");
   for (u_int i = 0; i < 20; i++) {
     printf("%d\n", Store::WordToInt(dict->GetItem(i)));
   }
   std::printf("done\n");
-
+  std::printf("Creating root set...\n");
+  Block *rb = Store::AllocBlock(Store::MakeLabel(0), 2);
+  Block *q  = Store::AllocBlock(Store::MakeLabel(0), 2048);
+  rb->InitArg(0, dict->ToWord());
+  rb->InitArg(1, q->ToWord());
+  word root = rb->ToWord();
+  std::printf("done\n");
+  Store::MemStat();
+  std::printf("Forcing GC...\n");
+  Store::ForceGC(root, 0);
+  std::printf("done\n");
+  Store::MemStat();
+  rb = Store::WordToBlock(root);
+  if (WeakDictionary::FromWord(rb->GetArg(0))->IsMember(20)) {
+    std::printf("Finalization failed\n");
+  }
+  else {
+    std::printf("Finalization succeeded\n");
+  }
+  std::printf("Forcing GC again...\n");
+  Store::ForceGC(root, 1);
+  std::printf("done\n");
+  Store::MemStat();
   std::printf("It Succeeded\n");
   return 0;
 }
