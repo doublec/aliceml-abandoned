15d1f2b1b70e82af15417aa837fcab2f21706c67
bruni <none@none>
2001-04-09 10:54:05 +0100

    Adapted test of WKDs.

--- vm-stockwerk/test/store/basic/wk.cc
+++ vm-stockwerk/test/store/basic/wk.cc
@@ -21,35 +21,52 @@ void AssertOutline(const char *file, int line, const char *message) {
 }
 #endif
 
+class MyFinalization : public Finalization {
+public:
+  MyFinalization() {}
+  ~MyFinalization() {}
+
+  void Finalize(word value) {
+    std::printf("MyFinalization::Finalize enter\n");
+    Block *p = Store::WordToBlock(value);
+    if (p->GetLabel() == WEAK_DICT_LABEL) {
+      std::printf("Got WK Dictionary\n");
+    }
+    else {
+      std::printf("Got something different\n");
+    }
+    std::printf("MyFinalizatio::Finalize leave\n");
+    std::fflush(stdout);
+  }
+};
+
 int main(void) {
   u_int memLimits[STORE_GENERATION_NUM];
-  WeakDictionary *dict;
+  WeakDictionary *d1, *d2;
 
   for (u_int i = 0; i < STORE_GENERATION_NUM; i++) {
     memLimits[i] = (i + 1) * STORE_MEMCHUNK_SIZE;
   }
   Store::InitStore(memLimits, 75, 20);
-  
-  std::printf("Allocating WD...\n");
-  dict = WeakDictionary::New(10, INVALID_POINTER);
+  Store::RegisterFinalizer(new MyFinalization());
+  std::printf("Allocating WDs...\n");
+  d1 = WeakDictionary::New(10);
+  d2 = WeakDictionary::New(10);
   std::printf("done\n");
   std::printf("Inserting Items...\n");
-  for (u_int i = 0; i < 20; i++) {
-    dict->InsertItem(i, Store::IntToWord(1000 * (i + 1)));
-  }
-  Block *p = Store::AllocBlock(Store::MakeLabel(0), 2048);
-  dict->InsertItem(20, p->ToWord());
-  std::printf("done\n");
-  std::printf("Accessing Items...\n");
-  for (u_int i = 0; i < 20; i++) {
-    printf("%d\n", Store::WordToInt(dict->GetItem(i)));
+  for (u_int i = 0; i < 1; i++) {
+    word p = Store::AllocBlock(Store::MakeLabel(0), 1024)->ToWord();
+
+    d1->InsertItem(i, p);
+    d2->InsertItem(i, p);
   }
   std::printf("done\n");
   std::printf("Creating root set...\n");
-  Block *rb = Store::AllocBlock(Store::MakeLabel(0), 2);
+  Block *rb = Store::AllocBlock(Store::MakeLabel(0), 3);
   Block *q  = Store::AllocBlock(Store::MakeLabel(0), 2048);
-  rb->InitArg(0, dict->ToWord());
-  rb->InitArg(1, q->ToWord());
+  rb->InitArg(0, d1->ToWord());
+  rb->InitArg(1, d2->ToWord());
+  rb->InitArg(2, q->ToWord());
   word root = rb->ToWord();
   std::printf("done\n");
   Store::MemStat();
@@ -58,7 +75,8 @@ int main(void) {
   std::printf("done\n");
   Store::MemStat();
   rb = Store::WordToBlock(root);
-  if (WeakDictionary::FromWord(rb->GetArg(0))->IsMember(20)) {
+  if ((WeakDictionary::FromWord(rb->GetArg(0))->IsMember(0)) ||
+      (WeakDictionary::FromWord(rb->GetArg(1))->IsMember(0))) {
     std::printf("Finalization failed\n");
   }
   else {
