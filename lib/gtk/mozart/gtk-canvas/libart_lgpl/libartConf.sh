84132f8bea1ee35029265fa60e3719e5c651717f
rossberg <none@none>
2001-04-17 13:40:23 +0100

    * Implemented usage inference for imports. * Simple implementation of import stripping: discard unused imports   (don't strip substructures yet, because that requires fixing all   longid sig annotations in the AST and is much more difficult to do right   in presence of a more flexible import discipline). * Added compiler flags to warn about unused imports (pretty useless   since it warns you about all the standard basis...) and to suppress   import discards. * Leif probably has to clean up the flattening phase.

--- lib/gtk/mozart/gtk-canvas/libart_lgpl/libartConf.sh
+++ lib/gtk/mozart/gtk-canvas/libart_lgpl/libartConf.sh
@@ -1,7 +1,7 @@
 #
 # Configuration file for using the LIBART library in GNOME applications
 #
-LIBART_LIBDIR="-L/home/thor/devel/stockhausen/lib/gtk/build/lib"
+LIBART_LIBDIR="-L/home/rossberg/src/stockhausen/lib/gtk/build/lib"
 LIBART_LIBS="-lart_lgpl"
-LIBART_INCLUDEDIR="-I/home/thor/devel/stockhausen/lib/gtk/build/include"
+LIBART_INCLUDEDIR="-I/home/rossberg/src/stockhausen/lib/gtk/build/include"
 MODULE_VERSION="libart-@VERSION@"
