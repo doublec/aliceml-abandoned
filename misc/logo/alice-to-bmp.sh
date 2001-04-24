c2f1f94cc9b5bc82259a9e6c94f118aa1ae565dc
kornstae <none@none>
2001-04-24 13:49:27 +0100

    Variants of the logo for different places.

--- misc/logo/alice-to-bmp.sh
+++ misc/logo/alice-to-bmp.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-mpost alice || exit 1
-
 gs="gs -q -dBATCH -dNOPAUSE -sDEVICE=bmp16m -r300"
 
-$gs -g2096x661 -sOutputFile=alice-strikethrough-shadow-rainbow.bmp alice.1
+process() {
+    mpost $1 || exit 1
+    $gs -g"$2"x"$3" -sOutputFile="$1"-"$2"x"$3".bmp "$1".1
+}
+
+(
+    read name xsize ysize
+    while [ ! -z "$name" ]
+    do
+	process "$name" "$xsize" "$ysize"
+	read name xsize ysize
+    done
+) <<EOF
+alice-ps 2096 661
+alice-strikethrough-shadow-blue 2096 661
+alice-strikethrough-shadow-rainbow 2096 661
+EOF
