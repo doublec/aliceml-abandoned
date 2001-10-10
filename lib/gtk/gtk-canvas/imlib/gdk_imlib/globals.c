448a7751ff500aadb4d9c8bba4b24408366cdf05
bruni <none@none>
2001-10-10 14:18:25 +0100

    Removed obsolete dirs ckit and gtk-canvas

--- lib/gtk/gtk-canvas/imlib/gdk_imlib/globals.c
+++
@@ -1,8 +0,0 @@
-
-#define _GNU_SOURCE
-#include <config.h>
-#include "gdk_imlib.h"
-#include "gdk_imlib_private.h"
-
-ImlibData	_gdk_real_imlib_data = { 0, 0, };
-ImlibData          *_gdk_imlib_data = &_gdk_real_imlib_data;
--- lib/gtk/mozart/gtk-canvas/libart_lgpl/libartConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the LIBART library in GNOME applications
-#
-LIBART_LIBDIR="-L/home/rossberg/src/stockhausen/lib/gtk/build/lib"
-LIBART_LIBS="-lart_lgpl"
-LIBART_INCLUDEDIR="-I/home/rossberg/src/stockhausen/lib/gtk/build/include"
-MODULE_VERSION="libart-@VERSION@"
