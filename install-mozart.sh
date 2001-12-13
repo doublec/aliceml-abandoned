54859f5826f39ee32864f24c4fef871140c0bc71
rossberg <none@none>
2001-12-13 15:51:11 +0000

    * Configurable options for different builds. * Fixed build process for Gtk lib.

--- install-mozart.sh
+++ install-mozart.sh
@@ -18,12 +18,16 @@ echo Trying to install Stockhausen to $prefix...
 
 PLATFORM=x86-linux
 
-rm -f bootstrap/alicec-mozart.$PLATFORM bootstrap/alicedep.$PLATFORM
+OPTS1= # '--dump-phases' # --dump-abstraction-result' # --dump-intermediate'
+OPTS2=
+OPTS3=--dump-phases # --dump-intermediate'
+
+rm -f bootstrap/alicec-mozart.$PLATFORM #bootstrap/alicedep.$PLATFORM
 (cd bootstrap && make) || exit 1
 (cd vm-mozart && make depend) || exit 1
-(cd vm-mozart && /usr/bin/time -po /tmp/time1.out make build1-install) || exit 1
-(cd vm-mozart && make build2-install) || exit 1
-(cd vm-mozart && /usr/bin/time -po /tmp/time3.out make build3-install) || exit 1
+(cd vm-mozart && /usr/bin/time -po /tmp/time1.out make ALICEC_EXTRA_OPTS="$OPTS1" build1-install) || exit 1
+(cd vm-mozart && make ALICEC_EXTRA_OPTS="$OPTS2" build2-install) || exit 1
+(cd vm-mozart && /usr/bin/time -po /tmp/time3.out make ALICEC_EXTRA_OPTS="$OPTS3" build3-install) || exit 1
 (cd vm-mozart && make PREFIX=$prefix install) || exit 1
 PATH=$prefix/bin:$PATH
 export PATH
@@ -34,8 +38,9 @@ unset STOCKHOME
 (cd lib/constraints && make all PREFIX=$prefix install) || exit 1
 (cd lib/distribution && make depend) || exit 1
 (cd lib/distribution && make all PREFIX=$prefix install) || exit 1
+(cd lib/gtk && autoconf && configure --with-gtk-canvas-dir=/opt/gtk-canvas) || exit 1
 #(cd lib/gtk && make depend) || exit 1
-#(cd lib/gtk && make all PREFIX=$prefix install) || exit 1
+(cd lib/gtk && make all PREFIX=$prefix install) || exit 1
 
 echo Done.
 echo Time for build 1:
