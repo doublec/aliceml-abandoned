6aa33004d9ca8fb5079da742ff83e9447bba7086
kornstae <none@none>
2002-02-26 09:28:22 +0000

    Added realToVector.

--- vm-stockwerk/emulator/UnsafeReflect.cc
+++ vm-stockwerk/emulator/UnsafeReflect.cc
@@ -1,9 +1,11 @@
 //
 // Authors:
 //   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
 //
 // Copyright:
 //   Thorsten Brunklaus, 2002
+//   Leif Kornstaedt, 2002
 //
 // Last Change:
 //   $Date$ by $Author$
@@ -12,13 +14,30 @@
 
 #include "emulator/Authoring.hh"
 
+static bool littleEndian;
+
+union FloatChunk {
+  unsigned char c[sizeof(double)];
+  int i[sizeof(double) / sizeof(int)];
+  double d;
+};
+
 DEFINE1(UnsafeReflect_cast) {
   RETURN(x0);
 } END
 
 DEFINE1(UnsafeReflect_realToVector) {
-  Vector *vector = Vector::New(0);
-  // to be done
+  DECLARE_REAL(r, x0);
+  Vector *vector = Vector::New(sizeof(double));
+  FloatChunk x;
+  x.d = r->GetValue();
+  if (littleEndian) {
+    for (u_int i = 0; i < sizeof(double); i++)
+      vector->Init(i, Store::IntToWord(x.c[sizeof(double) - 1 - i]));
+  } else {
+    for (u_int i = 0; i < sizeof(double); i++)
+      vector->Init(i, Store::IntToWord(x.c[i]));
+  }
   RETURN(vector->ToWord());
 } END
 
@@ -49,6 +68,15 @@ DEFINE1(UnsafeReflect_UnreflectSig) {
 } END
 
 word UnsafeReflect(void) {
+  FloatChunk x;
+  x.i[0] = 1;
+  for (u_int i = 1; i < sizeof(double) / sizeof(int); i++)
+    x.i[i] = 0;
+  if (x.c[0] == 1)
+    littleEndian = true;
+  else
+    littleEndian = false;
+
   Tuple *t = Tuple::New(6);
   t->Init(0, Primitive::MakeFunction(UnsafeReflect_cast, 1));
   t->Init(1, Primitive::MakeFunction(UnsafeReflect_realToVector, 1));
