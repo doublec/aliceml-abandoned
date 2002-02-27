78ff94063df617ecfa27ba8128f89e201c151b2f
bruni <none@none>
2002-02-27 10:35:18 +0000

    Fixed REQUEST handling

--- vm-seam/alice/primitives/Unqualified.cc
+++ vm-seam/alice/primitives/Unqualified.cc
@@ -81,6 +81,7 @@ static int Compare(word x0, word x1) {
 DEFINE2(opeq) {
   int result = Compare(x0, x1);
   if (result == -1) {
+    taskStack->PushFrame(prim_self);
     return Interpreter::REQUEST;
   }
   else {
@@ -91,6 +92,7 @@ DEFINE2(opeq) {
 DEFINE2(opnoteq) {
   int result = Compare(x0, x1);
   if (result == -1) {
+    taskStack->PushFrame(prim_self);
     return Interpreter::REQUEST;
   }
   else {
