2f344012d11dd17962b674d213674bd65850a21f
bruni <none@none>
2002-02-27 15:58:14 +0000

    Use print depth and width; added labels.

--- generic/TaskStack.hh
+++ generic/TaskStack.hh
@@ -31,6 +31,7 @@ public:
   using Stack::ToWord;
   using Stack::IsEmpty;
   // TaskStack Functions
+  static u_int maxWidth, maxDepth;
   static void Dump(word x);
   void DumpTaskStack();
   void PushFrame(word frame) {
