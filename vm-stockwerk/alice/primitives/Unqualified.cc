5465f55b2d892db4af6135a4e69e2dfa0da70416
bruni <none@none>
2002-02-27 17:19:24 +0000

    Fixed equality: TagVals were missing; nicer.

--- vm-stockwerk/alice/primitives/Unqualified.cc
+++ vm-stockwerk/alice/primitives/Unqualified.cc
@@ -13,69 +13,71 @@
 //
 
 #include "emulator/Authoring.hh"
+#include <cstdio>
 
  // NON-ABSTRACT TASK STACK USE
 static int Compare(word x0, word x1) {
   Block *a = Store::WordToBlock(x0);
   Block *b = Store::WordToBlock(x1);
-  if (a == INVALID_POINTER) { // a is Transient or int
+  if (a == INVALID_POINTER) { // x0 is Transient or int
     int i = Store::WordToInt(x0);
-    if (i == INVALID_INT) {
+    if (i == INVALID_INT) { // x0 is transient
       Scheduler::currentData = x0;
       return -1;
     }
-    if (b != INVALID_POINTER) { // b is TagVal
+    // x0 is int
+    if (b != INVALID_POINTER) // x1 is Block
       return 0;
-    }
+    // x1 is Transient or int
     int j = Store::WordToInt(x1);
-    if (j == INVALID_INT) {
+    if (j == INVALID_INT) { // x1 is Transient
       Scheduler::currentData = x1;
       return -1;
     }
     return (i == j);
   }
-  if (b == INVALID_POINTER) { // b is Transient or int, but a is a block
+  if (b == INVALID_POINTER) { // x1 is Transient or int, but x0 is a block
     int j = Store::WordToInt(x1);
-    if (j == INVALID_INT) {
+    if (j == INVALID_INT) { // x1 is Transient
       Scheduler::currentData = x1;
       return -1;
     }
     return 0;
   }
-  // from here, both a and b are blocks
+  // from here, both x0 and x1 are blocks
   BlockLabel label = a->GetLabel();
+  if (label != b->GetLabel())
+    return 0;
   switch (label) {
   case TUPLE_LABEL:
   case Alice::ConVal:
   case Alice::Vector:
     {
+    structural_equality:
       u_int size = a->GetSize();
       if (b->GetSize() != size)
 	return 0;
-      for (u_int i = 1; i <= size; i++) {
+      for (u_int i = 0; i < size; i++) { // request left-to-right
 	int result = Compare(a->GetArg(i), b->GetArg(i));
-	if (result != 1) {
+	if (result != 1)
 	  return result;
-	}
       }
-    }
-    return 1;
-  case CHUNK_LABEL:
-    {
-      u_int size = a->GetSize();
-      if (b->GetSize() != size)
-	return 0;
-      for (u_int i = 1; i <= size; i++)
-	if (a->GetArg(i) != b->GetArg(i))
-	  return 0;
       return 1;
     }
-  default:
-    if (label == b->GetLabel()) {
-      return (a == b);
+  case CHUNK_LABEL:
+    {
+      Chunk *ac = (Chunk *) a;
+      Chunk *bc = (Chunk *) b;
+      u_int size = ac->GetSize();
+      return bc->GetSize() == size &&
+	!memcmp(ac->GetBase(), bc->GetBase(), size);
     }
+  default:
+    if (Alice::IsTag(label)) {
+      goto structural_equality;
+    } else // fall back to identity-based equality
+      return (a == b);
   }
-  return 0;
 }
 
 DEFINE2(opeq) {
@@ -85,6 +87,9 @@ DEFINE2(opeq) {
     return Interpreter::REQUEST;
   }
   else {
+    fprintf(stderr, "§§§§§§§§§§§§§§§§§§§§\n");
+    TaskStack::Dump(x0);
+    TaskStack::Dump(x1);
     RETURN_BOOL(result);
   }
 } END
