c0fe658b2110dad5664520db01dbdcdb55616140
kornstae <none@none>
2002-02-28 11:37:19 +0000

    Need #pragma implementation for some header files.

---
+++ generic/Outline.cc
@@ -0,0 +1,25 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#if defined(INTERFACE)
+#pragma implementation "emulator/ConcreteCode.hh"
+#pragma implementation "emulator/Closure.hh"
+#pragma implementation "emulator/Thread.hh"
+#pragma implementation "emulator/ThreadQueue.hh"
+#pragma implementation "emulator/Tuple.hh"
+#endif
+
+#include "emulator/ConcreteCode.hh"
+#include "emulator/Closure.hh"
+#include "emulator/Thread.hh"
+#include "emulator/ThreadQueue.hh"
+#include "emulator/Tuple.hh"
