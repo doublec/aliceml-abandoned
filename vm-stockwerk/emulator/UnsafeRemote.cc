beae41ba22ac8edd080406893db067c6566dcdfb
kornstae <none@none>
2002-03-11 15:44:19 +0000

    Added getLocalIP.

--- vm-stockwerk/emulator/UnsafeRemote.cc
+++ vm-stockwerk/emulator/UnsafeRemote.cc
@@ -10,10 +10,47 @@
 //   $Revision$
 //
 
+#if defined(__MINGW32__) || defined(_MSC_VER)
+#include <winsock.h>
+#else
+#include <netdb.h>
+#include <sys/utsname.h>
+#include <arpa/inet.h>
+#endif
+
 #include "emulator/Authoring.hh"
 #include "emulator/RootSet.hh"
 #include "emulator/Pickler.hh"
 #include "emulator/Unpickler.hh"
+
+DEFINE0(UnsafeRemote_getLocalIP) {
+#if defined(__MINGW32__) || defined(_MSC_VER)
+  char nodeName[256];
+  int ret = gethostname(nodeName, sizeof(nodeName));
+  if (ret < 0) {
+    Error("GetLocalIP");
+  }
+#else
+  utsname unameBuffer;
+  int ret = uname(&unameBuffer);
+  if (ret < 0) {
+    Error("GetLocalIP");
+  }
+  char *nodeName = &unameBuffer.nodename[0];
+#endif
+
+  hostent *entry = gethostbyname(nodeName);
+  if (!entry) {
+    entry = gethostbyname("localhost");
+    if (!entry) {
+      Error("GetLocalIP");
+    }
+  }
+
+  in_addr addr;
+  std::memcpy(&addr, entry->h_addr_list[0], sizeof(addr));
+  RETURN(String::New(inet_ntoa(addr))->ToWord());
+} END
 
 static word callback;
 
@@ -47,14 +84,16 @@ word UnsafeRemote(void) {
   RootSet::Add(callback);
   callback = Store::IntToWord(0);
 
-  Tuple *t = Tuple::New(4);
+  Tuple *t = Tuple::New(5);
   t->Init(0, Primitive::MakeClosure("UnsafeRemote_dynamicCall",
 				    UnsafeRemote_dynamicCall, 2, false));
-  t->Init(1, Primitive::MakeClosure("UnsafeRemote_pack",
+  t->Init(1, Primitive::MakeClosure("UnsafeRemote_getLocalIP",
+				    UnsafeRemote_getLocalIP, 0, true));
+  t->Init(2, Primitive::MakeClosure("UnsafeRemote_pack",
 				    UnsafeRemote_pack, 1, true));
-  t->Init(2, Primitive::MakeClosure("UnsafeRemote_setCallback",
+  t->Init(3, Primitive::MakeClosure("UnsafeRemote_setCallback",
 				    UnsafeRemote_setCallback, 1, true));
-  t->Init(3, Primitive::MakeClosure("UnsafeRemote_unpack",
+  t->Init(4, Primitive::MakeClosure("UnsafeRemote_unpack",
 				    UnsafeRemote_unpack, 1, true));
   RETURN_STRUCTURE(t);
 }
