75eb1905eab724286de1209f14853c896fda1a09
bruni <none@none>
2002-03-23 21:09:12 +0000

    Minor changes to HashTable

--- vm-stockwerk/adt/HashTable.hh
+++ vm-stockwerk/adt/HashTable.hh
@@ -18,44 +18,24 @@
 
 #include "store/Store.hh"
 
-class HashTable : private WeakDictionary {
+class HashTable : public BlockHashTable {
 public:
   enum hashkeytype {
     INT_KEY,
     BLOCK_KEY
   };
 
-  using Block::ToWord;
-  using WeakDictionary::GetSize;
-  using WeakDictionary::Clear;
-  using WeakDictionary::IsEmpty;
-
-  void InsertItem(word key, word value) {
-    WeakDictionary::InsertItem(key, value);
-  }
-  void DeleteItem(word key) {
-    WeakDictionary::DeleteItem(key);
-  }
-  int IsMember(word key) {
-    return WeakDictionary::IsMember(key);
-  }
-  word GetItem(word key) {
-    return WeakDictionary::GetItem(key); // must be member
-  }
-
   static HashTable *New(hashkeytype type, u_int size) {
     return (HashTable *) WeakDictionary::New((WeakDictionary::hashkeytype) type,
-					     HASHTABLE_LABEL, size);
+					     HASHTABLE_LABEL, size, NULL);
   }
   static HashTable *FromWord(word x) {
     Block *p = Store::WordToBlock(x);
-
     Assert((p == INVALID_POINTER) || (p->GetLabel() == HASHTABLE_LABEL));
     return (HashTable *) p;
   }
   static HashTable *FromWordDirect(word x) {
     Block *p = Store::DirectWordToBlock(x);
-
     Assert(p->GetLabel() == HASHTABLE_LABEL);
     return (HashTable *) p;
   }
