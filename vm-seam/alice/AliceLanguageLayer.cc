930c87eb68434a3aacc64c93ab2ee0aa4ad83a4c
kornstae <none@none>
2002-04-18 17:25:26 +0100

    Moved handlers for Alice.primitive.value and Alice.primitive.function    into language layer.

--- vm-seam/alice/AliceLanguageLayer.cc
+++ vm-seam/alice/AliceLanguageLayer.cc
@@ -24,8 +24,24 @@
 #include "alice/AbstractCodeInterpreter.hh"
 #include "alice/AliceConcreteCode.hh"
 
-word AliceLanguageLayer::functionTransformName;
-word AliceLanguageLayer::constructorTransformName;
+word AliceLanguageLayer::TransformNames::primitiveValue;
+word AliceLanguageLayer::TransformNames::primitiveFunction;
+word AliceLanguageLayer::TransformNames::function;
+word AliceLanguageLayer::TransformNames::constructor;
+
+static word AlicePrimitiveValueHandler(word x) {
+  TagVal *tagVal = TagVal::FromWordDirect(x);
+  Assert(tagVal->GetTag() == 0);
+  tagVal->AssertWidth(1);
+  return PrimitiveTable::LookupValue(Chunk::FromWordDirect(tagVal->Sel(0)));
+}
+
+static word AlicePrimitiveFunctionHandler(word x) {
+  TagVal *tagVal = TagVal::FromWordDirect(x);
+  Assert(tagVal->GetTag() == 0);
+  tagVal->AssertWidth(1);
+  return PrimitiveTable::LookupFunction(Chunk::FromWordDirect(tagVal->Sel(0)));
+}
 
 static word AliceFunctionHandler(word x) {
   return AliceConcreteCode::New(TagVal::FromWordDirect(x))->ToWord();
@@ -35,20 +51,32 @@ static word AliceConstructorHandler(word x) {
   Tuple *tuple = Tuple::FromWordDirect(x);
   tuple->AssertWidth(2);
   Constructor *constructor =
-    Constructor::New(tuple->Sel(0), Store::WordToBlock(tuple->Sel(1)));
+    Constructor::New(tuple->Sel(0), Store::DirectWordToBlock(tuple->Sel(1)));
   return constructor->ToWord();
 }
 
 void AliceLanguageLayer::Init() {
+  String *alicePrimitiveValue = String::New("Alice.primitive.value");
+  TransformNames::primitiveValue = alicePrimitiveValue->ToWord();
+  RootSet::Add(TransformNames::primitiveValue);
+  Unpickler::RegisterHandler(static_cast<Chunk *>(alicePrimitiveValue),
+			     AlicePrimitiveValueHandler);
+
+  String *alicePrimitiveFunction = String::New("Alice.primitive.function");
+  TransformNames::primitiveFunction = alicePrimitiveFunction->ToWord();
+  RootSet::Add(TransformNames::primitiveFunction);
+  Unpickler::RegisterHandler(static_cast<Chunk *>(alicePrimitiveFunction),
+			     AlicePrimitiveFunctionHandler);
+
   String *aliceFunction = String::New("Alice.function");
-  functionTransformName = aliceFunction->ToWord();
-  RootSet::Add(functionTransformName);
+  TransformNames::function = aliceFunction->ToWord();
+  RootSet::Add(TransformNames::function);
   Unpickler::RegisterHandler(static_cast<Chunk *>(aliceFunction),
 			     AliceFunctionHandler);
 
   String *aliceConstructor = String::New("Alice.constructor");
-  constructorTransformName = aliceConstructor->ToWord();
-  RootSet::Add(constructorTransformName);
+  TransformNames::constructor = aliceConstructor->ToWord();
+  RootSet::Add(TransformNames::constructor);
   Unpickler::RegisterHandler(static_cast<Chunk *>(aliceConstructor),
 			     AliceConstructorHandler);
 
