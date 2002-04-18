bc8867c1699e6c8c58210e63db3a7b5098e1f1ca
kornstae <none@none>
2002-04-18 16:19:37 +0100

    Centralize all initialization of the Alice language layer.

---
+++ vm-seam/alice/AliceLanguageLayer.hh
@@ -0,0 +1,27 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#ifndef __ALICE__ALICE_LANGUAGE_LAYER_HH__
+#define __ALICE__ALICE_LANGUAGE_LAYER_HH__
+
+#if defined(INTERFACE)
+#pragma interface "alice/AliceLanguageLayer.hh"
+#endif
+
+class AliceLanguageLayer {
+public:
+  static word functionTransformName;
+
+  static void Init();
+};
+
+#endif
