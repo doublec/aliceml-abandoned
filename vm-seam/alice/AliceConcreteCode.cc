4b302bce67e79b552627e9c47639794094bcf416
kornstae <none@none>
2002-04-18 16:19:37 +0100

    Centralize all initialization of the Alice language layer.

--- vm-seam/alice/AliceConcreteCode.cc
+++ vm-seam/alice/AliceConcreteCode.cc
@@ -14,21 +14,15 @@
 #pragma implementation "alice/AliceConcreteCode.hh"
 #endif
 
-#include "generic/RootSet.hh"
 #include "generic/Transform.hh"
 #include "alice/AliceConcreteCode.hh"
-
-static word aliceFunctionTransformName;
-
-void AliceConcreteCode::Init() {
-  aliceFunctionTransformName = String::New("Alice.function")->ToWord();
-  RootSet::Add(aliceFunctionTransformName);
-}
+#include "alice/AliceLanguageLayer.hh"
 
 AliceConcreteCode *AliceConcreteCode::New(TagVal *abstractCode) {
   ConcreteCode *concreteCode =
     ConcreteCode::New(AbstractCodeInterpreter::self, SIZE);
-  Chunk *name = Store::DirectWordToChunk(aliceFunctionTransformName);
+  Chunk *name =
+    Store::DirectWordToChunk(AliceLanguageLayer::functionTransformName);
   Transform *transform = Transform::New(name, abstractCode->ToWord());
   concreteCode->Init(ABSTRACT_CODE_POS, abstractCode->ToWord());
   concreteCode->Init(TRANSFORM_POS, transform->ToWord());
