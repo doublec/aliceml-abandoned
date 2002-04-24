acfbafabea1450f882019385ad4617a297dbda71
bruni <none@none>
2002-04-24 13:20:54 +0100

    Updated windows scripts

--- vm-mozart/alicerun.bat
+++ vm-mozart/alicerun.bat
@@ -1,10 +1,9 @@
 @echo off
 
-set OZHOME=C:\Program Files\Mozart
-set STOCKHOME=Y:\.root\opt\stockhausen-operette2
-
 set OZ_LOAD=pattern=?{x}=?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x};cache=%OZHOME%/cache
 
-set PATH=%OZHOME%\bin;%PATH%
+set ALICE_LOAD=pattern=x-oz:?{x}=x-oz:?{x};pattern=?{x}=?{x}.ozf;pattern=?{x}=?{x};pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}
+
+set OZ_LOAD=%ALICE_LOAD%;%OZ_LOAD%
 
 ozengine x-alice:/VMMain %1 %2 %3 %4 %5 %6 %7 %8 %9
