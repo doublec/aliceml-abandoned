aecef1583312e2181d59c30f8016c918a84d7bc9
kornstae <none@none>
2002-05-07 14:47:47 +0100

    distclean instead of clean.

--- clean.sh
+++ clean.sh
@@ -2,8 +2,8 @@
 
 rm -f bootstrap/alicec-mozart.$PLATFORM bootstrap/alicedep.$PLATFORM
 (cd bootstrap && make clean) || exit 1
-(cd vm-mozart && make clean) || exit 1
-(cd lib/inspector && make clean) || exit 1
-(cd lib/constraints && make clean) || exit 1
-(cd lib/distribution && make clean) || exit 1
-(cd lib/gtk && make clean) || exit 1
+(cd vm-mozart && make distclean) || exit 1
+(cd lib/inspector && make distclean) || exit 1
+(cd lib/constraints && make distclean) || exit 1
+(cd lib/distribution && make distclean) || exit 1
+(cd lib/gtk && [ -f Makefile ] && make distclean) || exit 1
