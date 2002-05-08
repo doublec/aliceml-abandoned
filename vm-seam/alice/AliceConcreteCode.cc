06d5366cff223af579d507de95ad7b4504a774f9
kornstae <none@none>
2002-05-08 14:49:34 +0100

    Merged from branch new-liveness-analysis.

--- vm-seam/alice/AliceConcreteCode.cc
+++ vm-seam/alice/AliceConcreteCode.cc
@@ -15,6 +15,7 @@
 #endif
 
 #include "generic/Transform.hh"
+#include "alice/AbstractCode.hh"
 #include "alice/AliceConcreteCode.hh"
 #include "alice/AliceLanguageLayer.hh"
 
@@ -27,4 +28,16 @@ AliceConcreteCode *AliceConcreteCode::New(TagVal *abstractCode) {
   concreteCode->Init(ABSTRACT_CODE_POS, abstractCode->ToWord());
   concreteCode->Init(TRANSFORM_POS, transform->ToWord());
   return static_cast<AliceConcreteCode *>(concreteCode);
+}
+
+void AliceConcreteCode::Disassemble(std::FILE *file) {
+  Transform *transform = Transform::FromWordDirect(Get(TRANSFORM_POS));
+  TagVal *abstractCode = TagVal::FromWordDirect(transform->GetArgument());
+  Tuple *coord = Tuple::FromWordDirect(abstractCode->Sel(0));
+  fprintf(file, "Disassembling function at %s:%d.%d\n\n",
+	  String::FromWordDirect(coord->Sel(0))->ExportC(),
+	  Store::DirectWordToInt(coord->Sel(1)),
+	  Store::DirectWordToInt(coord->Sel(2)));
+  TagVal *pc = TagVal::FromWordDirect(abstractCode->Sel(4));
+  AbstractCode::Disassemble(file, pc);
 }
