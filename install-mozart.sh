af7dec6bc691603cf71af8bdcc101e3a3558929d
kornstae <none@none>
2002-05-14 15:39:01 +0100

    Renamed install.sh to install-mozart.sh;    install-stockwerk.sh is soon to come;    install.sh now invokes install-mozart.sh.

--- install-mozart.sh
+++ install-mozart.sh
@@ -22,12 +22,15 @@ OPTS1= # '--dump-phases' # --dump-abstraction-result' # --dump-intermediate'
 OPTS2= # '--dump-phases'
 OPTS3= # '--dump-phases' # --dump-intermediate'
 
+TIMEDIR=`pwd`/time
+export TIMEDIR
+
 rm -f bootstrap/alicec-mozart.$PLATFORM #bootstrap/alicedep.$PLATFORM
 (cd bootstrap && make) || exit 1
 (cd vm-mozart && make depend) || exit 1
-(cd vm-mozart && /usr/bin/time -po /tmp/time1.out make ALICEC_EXTRA_OPTS="$OPTS1" build1-install) || exit 1
+(cd vm-mozart && /usr/bin/time -po ${TIMEDIR}1 make ALICEC_EXTRA_OPTS="$OPTS1" build1-install) || exit 1
 (cd vm-mozart && make ALICEC_EXTRA_OPTS="$OPTS2" build2-install) || exit 1
-(cd vm-mozart && /usr/bin/time -po /tmp/time3.out make ALICEC_EXTRA_OPTS="$OPTS3" build3-install) || exit 1
+(cd vm-mozart && /usr/bin/time -po ${TIMEDIR}3 make ALICEC_EXTRA_OPTS="$OPTS3" build3-install) || exit 1
 (cd vm-mozart && make PREFIX=$prefix install) || exit 1
 PATH=$prefix/bin:$PATH
 export PATH
@@ -38,12 +41,12 @@ unset STOCKHOME
 (cd lib/constraints && make all PREFIX=$prefix install) || exit 1
 (cd lib/distribution && make depend) || exit 1
 (cd lib/distribution && make all PREFIX=$prefix install) || exit 1
-(cd lib/gtk && autoconf && configure --with-gtk-canvas-dir=/opt/gtk-canvas) || exit 1
+(cd lib/gtk && autoconf && ./configure --with-gtk-canvas-dir=/opt/gtk-canvas) || exit 1
 #(cd lib/gtk && make depend) || exit 1
 (cd lib/gtk && make all PREFIX=$prefix install) || exit 1
 
 echo Done.
 echo Time for build 1:
-cat /tmp/time1.out
+cat ${TIMEDIR}1
 echo Time for build 3:
-cat /tmp/time3.out
+cat ${TIMEDIR}3
