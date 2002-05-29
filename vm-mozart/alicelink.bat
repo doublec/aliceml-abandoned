135d1864a73b708ed1027594c49452af0ec515fb
kornstae <none@none>
2002-05-29 10:57:14 +0100

    Use the Alice linker instead of Oz linker.

--- vm-mozart/alicelink.bat
+++ vm-mozart/alicelink.bat
@@ -1,5 +1,2 @@
 @echo off
-
-set OZ_LOAD=pattern=?{x}=?{x}.ozf:pattern=x-alice:/?{x}=$STOCKHOME/?{x}.ozf:$OZ_LOAD
-
-alicerun "%OZHOME%/bin/ozl.exe" %1 %2 %3 %4 %5 %6 %7 %8 %9
+alicerun x-alice:/linker/Main %1 %2 %3 %4 %5 %6 %7 %8 %9
