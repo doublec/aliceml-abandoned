275c8c90aad94051e9d3918b321e3e128a324f61
kornstae <none@none>
2002-06-21 13:18:42 +0100

    Task stacks are cached within the scheduler,    to avoid an extra indirection.

--- vm-stockwerk/generic/Thread.hh
+++ vm-stockwerk/generic/Thread.hh
@@ -23,22 +23,14 @@
 class Thread: private Block {
   friend class Scheduler;
 public:
-  enum priority {
-    HIGH, NORMAL, LOW
-  };
-
-  enum state {
-    BLOCKED, RUNNABLE, TERMINATED
-  };
+  enum priority { HIGH, NORMAL, LOW };
+  enum state { BLOCKED, RUNNABLE, TERMINATED };
 private:
-  static const u_int PRIORITY_POS     = 0;
-  static const u_int TASK_STACK_POS   = 1;
-  static const u_int STATE_POS        = 2;
-  static const u_int IS_SUSPENDED_POS = 3;
-  static const u_int NARGS_POS        = 4;
-  static const u_int ARGS_POS         = 5;
-  static const u_int FUTURE_POS       = 6;
-  static const u_int SIZE             = 7;
+  enum {
+    PRIORITY_POS, STATE_POS, IS_SUSPENDED_POS,
+    TASK_STACK_POS, NFRAMES_POS, NARGS_POS, ARGS_POS,
+    FUTURE_POS, SIZE
+  };
 
   void SetState(state s) {
     ReplaceArg(STATE_POS, s);
@@ -54,12 +46,13 @@ public:
   using Block::ToWord;
 
   // Thread Constructor
-  static Thread *New(u_int nArgs, word args, TaskStack *taskStack) {
+  static Thread *New(u_int nArgs, word args) {
     Block *b = Store::AllocBlock(THREAD_LABEL, SIZE);
     b->InitArg(PRIORITY_POS, NORMAL);
-    b->InitArg(TASK_STACK_POS, taskStack->ToWord());
     b->InitArg(STATE_POS, RUNNABLE);
     b->InitArg(IS_SUSPENDED_POS, false);
+    b->InitArg(NFRAMES_POS, TaskStack::initialNumberOfFrames);
+    b->InitArg(TASK_STACK_POS, TaskStack::New()->ToWord());
     b->InitArg(NARGS_POS, nArgs);
     b->InitArg(ARGS_POS, args);
     b->InitArg(FUTURE_POS, 0);
@@ -78,25 +71,35 @@ public:
   }
 
   // Thread Accessors
-  TaskStack *GetTaskStack() { //--** should be private
-    return TaskStack::FromWordDirect(GetArg(TASK_STACK_POS));
+  priority GetPriority() {
+    return static_cast<priority>(Store::DirectWordToInt(GetArg(PRIORITY_POS)));
   }
-  void SetArgs(u_int nArgs, word args) {
-    ReplaceArg(NARGS_POS, nArgs);
-    ReplaceArg(ARGS_POS, args);
+  state GetState() {
+    return static_cast<state>(Store::DirectWordToInt(GetArg(STATE_POS)));
+  }
+  bool IsSuspended() {
+    return Store::DirectWordToInt(GetArg(IS_SUSPENDED_POS));
+  }
+  TaskStack *GetTaskStack(u_int &nFrames) {
+    nFrames = Store::DirectWordToInt(GetArg(NFRAMES_POS));
+    TaskStack *taskStack = TaskStack::FromWordDirect(GetArg(TASK_STACK_POS));
+#ifdef DEBUG_CHECK
+    ReplaceArg(TASK_STACK_POS, 0);
+#endif
+    return taskStack;
+  }
+  void SetTaskStack(TaskStack *taskStack, u_int nFrames) {
+    Assert(GetArg(TASK_STACK_POS) == Store::IntToWord(0));
+    ReplaceArg(NFRAMES_POS, nFrames);
+    ReplaceArg(TASK_STACK_POS, taskStack->ToWord());
   }
   word GetArgs(u_int &nArgs) {
     nArgs = Store::DirectWordToInt(GetArg(NARGS_POS));
     return GetArg(ARGS_POS);
   }
-  priority GetPriority() {
-    return static_cast<priority>(Store::DirectWordToInt(GetArg(PRIORITY_POS)));
-  }
-  bool IsSuspended() {
-    return Store::DirectWordToInt(GetArg(IS_SUSPENDED_POS));
-  }
-  state GetState() {
-    return static_cast<state>(Store::DirectWordToInt(GetArg(STATE_POS)));
+  void SetArgs(u_int nArgs, word args) {
+    ReplaceArg(NARGS_POS, nArgs);
+    ReplaceArg(ARGS_POS, args);
   }
   void SetTerminated() {
     SetArgs(0, Store::IntToWord(0));
@@ -114,8 +117,22 @@ public:
     ReplaceArg(FUTURE_POS, 0);
     SetState(RUNNABLE);
   }
+
+  // Task Stack Operations
+  void PushFrame(word frame) {
+    u_int nFrames = Store::DirectWordToInt(GetArg(NFRAMES_POS));
+    TaskStack *taskStack = TaskStack::FromWordDirect(GetArg(TASK_STACK_POS));
+    if (nFrames == taskStack->GetSize()) {
+      taskStack = taskStack->Enlarge();
+      ReplaceArg(TASK_STACK_POS, taskStack->ToWord());
+    }
+    Assert(nFrames < taskStack->GetSize());
+    taskStack->ReplaceArg(nFrames++, frame);
+    ReplaceArg(NFRAMES_POS, nFrames);
+  }
   void Purge() {
-    GetTaskStack()->Purge();
+    u_int nFrames = Store::DirectWordToInt(GetArg(NFRAMES_POS));
+    TaskStack::FromWordDirect(GetArg(TASK_STACK_POS))->Purge(nFrames);
   }
 };
 
