63a8f9311e0456308705c6406379966d21956181
bruni <none@none>
2002-06-26 15:19:24 +0100

    Preliminary optimisation checkin

--- vm-stockwerk/alice/JitterAliceData.hh
+++ vm-stockwerk/alice/JitterAliceData.hh
@@ -19,84 +19,86 @@
 namespace JITAlice {
   class Cell : public ::Cell {
   public:
-    static void New(u_int Ptr) {
-      JITStore::AllocBlock(Ptr, Alice::Cell, SIZE);
+    static void New(u_int This) {
+      JITStore::AllocBlock(This, Alice::Cell, SIZE);
     }
-    static void Put(u_int Ptr, u_int Value) {
-      JITStore::InitArg(Ptr, VAL_POS, Value);
+    static void Put(u_int This, u_int Value) {
+      JITStore::InitArg(This, VAL_POS, Value);
     }
-    static void Sel(u_int Dest, u_int Ptr) {
-      JITStore::GetArg(Dest, Ptr, VAL_POS);
+    static void Sel(u_int Dest, u_int This) {
+      JITStore::GetArg(Dest, This, VAL_POS);
     }
   };
 
   class ConVal : public ::ConVal {
   public:
-    static void New(u_int Ptr, u_int size) {
-      JITStore::AllocBlock(Ptr, Alice::ConVal, BASE_SIZE + size);
+    static void New(u_int This, u_int size) {
+      JITStore::AllocBlock(This, Alice::ConVal, BASE_SIZE + size);
     }
-    static void InitConstr(u_int Ptr, u_int Value) {
-      JITStore::InitArg(Ptr, CON_POS, Value);
+    static void InitConstr(u_int This, u_int Value) {
+      JITStore::InitArg(This, CON_POS, Value);
     }
-    static void GetConstructor(u_int Dest, u_int Ptr) {
-      JITStore::GetArg(Dest, Ptr, CON_POS);
+    static void GetConstructor(u_int Dest, u_int This) {
+      JITStore::GetArg(Dest, This, CON_POS);
     }
-    static void Sel(u_int Dest, u_int Ptr, u_int pos) {
-      JITStore::GetArg(Dest, Ptr, BASE_SIZE + pos);
+    static void Sel(u_int Dest, u_int This, u_int pos) {
+      JITStore::GetArg(Dest, This, BASE_SIZE + pos);
     }
-    static void Put(u_int Ptr, u_int pos, u_int Value) {
-      JITStore::InitArg(Ptr, BASE_SIZE + pos, Value);
+    static void Put(u_int This, u_int pos, u_int Value) {
+      JITStore::InitArg(This, BASE_SIZE + pos, Value);
     }
   };
 
   class TagVal {
   public:
-    static void New(u_int Ptr, u_int tag, u_int size) {
-      JITStore::AllocBlock(Ptr, Alice::TagToLabel(tag), size);
+    static void New(u_int This, u_int tag, u_int size) {
+      JITStore::AllocBlock(This, Alice::TagToLabel(tag), size);
     }
-    static void Sel(u_int Dest, u_int Ptr, u_int pos) {
-      JITStore::GetArg(Dest, Ptr, pos);
+    static void Sel(u_int Dest, u_int This, u_int pos) {
+      JITStore::GetArg(Dest, This, pos);
     }
-    static void Put(u_int Ptr, u_int pos, u_int Value) {
-      JITStore::InitArg(Ptr, pos, Value);
+    static void Put(u_int This, u_int pos, u_int Value) {
+      JITStore::InitArg(This, pos, Value);
     }
-    static void GetTag(u_int Ptr) {
-      JITStore::Block::GetLabel(JIT_R0, Ptr);
+    static void GetTag(u_int This) {
+      JITStore::Block::GetLabel(JIT_R0, This);
       jit_subi_ui(JIT_R0, JIT_R0, Alice::MIN_TAG);
     }
   };
 
   class Vector : public ::Vector {
   public:
-    static void New(u_int Ptr, u_int size) {
-      JITStore::AllocBlock(Ptr, Alice::Vector, BASE_SIZE + size);
-      jit_movi_ui(JIT_R0, size);
-      JITStore::IntToWord(JIT_R0, JIT_R0);
-      JITStore::InitArg(Ptr, LENGTH_POS, JIT_R0);
+    static void New(u_int This, u_int size) {
+      JITStore::AllocBlock(This, Alice::Vector, BASE_SIZE + size);
+      jit_movi_p(JIT_R0, Store::IntToWord(size));
+      JITStore::InitArg(This, LENGTH_POS, JIT_R0);
     }
-    static void GetLength(u_int Dest, u_int Ptr) {
-      JITStore::GetArg(Dest, Ptr, LENGTH_POS);
+    static void GetLength(u_int Dest, u_int This) {
+      JITStore::GetArg(Dest, This, LENGTH_POS);
     }
-    static void Sel(u_int Dest, u_int Ptr, u_int pos) {
-      JITStore::GetArg(Dest, Ptr, BASE_SIZE + pos);
+    static void Sel(u_int Dest, u_int This, u_int pos) {
+      JITStore::GetArg(Dest, This, BASE_SIZE + pos);
     }
-    static void Put(u_int Ptr, u_int pos, u_int Value) {
-      JITStore::InitArg(Ptr, BASE_SIZE + pos, Value);
+    static void Put(u_int This, u_int pos, u_int Value) {
+      JITStore::InitArg(This, BASE_SIZE + pos, Value);
     }
   };
 
   class Record : public ::Record {
   public:
-    static void New(u_int Ptr, u_int n) {
-      JITStore::AllocBlock(Ptr, Alice::Record, BASE_SIZE + n * 2);
-      jit_movi_ui(JIT_R0, Store::IntToWord(n));
-      JITStore::InitArg(Ptr, WIDTH_POS, JIT_R0);
+    static void New(u_int This, u_int n) {
+      JITStore::AllocBlock(This, Alice::Record, BASE_SIZE + n * 2);
+      jit_movi_p(JIT_R0, Store::IntToWord(n));
+      JITStore::InitArg(This, WIDTH_POS, JIT_R0);
     }
-    static void InitLabel(u_int Ptr, u_int i, u_int Label) {
-      JITStore::InitArg(Ptr, BASE_SIZE + i * 2, Label);
+    static void GetWidth(u_int Dest, u_int This) {
+      JITStore::GetArg(Dest, This, WIDTH_POS);
     }
-    static void InitValue(u_int Ptr, u_int i, u_int Value) {
-      JITStore::InitArg(Ptr, BASE_SIZE + i * 2 + 1, Value);
+    static void InitLabel(u_int This, u_int i, u_int Label) {
+      JITStore::InitArg(This, BASE_SIZE + i * 2, Label);
+    }
+    static void InitValue(u_int This, u_int i, u_int Value) {
+      JITStore::InitArg(This, BASE_SIZE + i * 2 + 1, Value);
     }
   };
 
