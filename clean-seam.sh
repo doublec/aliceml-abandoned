664a66f484e5c46178117de818c1ab40466b5904
kornstae <none@none>
2002-07-02 14:31:48 +0100

    Separated clean-mozart.sh and clean-stockwerk.sh;    clean.sh invokes both in sequence.

--- clean-seam.sh
+++ clean-seam.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-rm -f bootstrap/alicec-stockwerk.$PLATFORM
+
+rm -f bootstrap/alicec-stockwerk.$PLATFORM bootstrap/alicedep.$PLATFORM
 (cd bootstrap && make clean) || exit 1
 (cd vm-stockwerk && make clean) || exit
 (cd vm-stockwerk && make -f Makefile.bootstrap distclean) || exit 1
--- clean.sh
+++ clean.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-rm -f bootstrap/alicec-mozart.$PLATFORM bootstrap/alicedep.$PLATFORM
-(cd bootstrap && make clean) || exit 1
-(cd vm-mozart && make distclean) || exit 1
-(cd lib/inspector && make distclean) || exit 1
-(cd lib/constraints && make distclean) || exit 1
-(cd lib/distribution && make distclean) || exit 1
-(cd lib/gtk && [ -f Makefile ] && make distclean) || exit 1
+./clean-mozart.sh
+./clean-stockwerk.sh
