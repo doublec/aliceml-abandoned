21b1ec8ed661baa3e11d5c9b9f0cb284e7aedc99
kornstae <none@none>
2002-07-31 16:36:57 +0100

    Separated alarm handling from other signals -   makes for a much cleaner implementation; Use stronger types for method arguments and returns; Made time slice duration configurable; Unix: timer is not reconfigured at every invocation (unnecessary); Alarms are kept in a sorted list, to make wakeup fast; Minor bug fixes.

--- generic/SignalHandler.cc
+++ generic/SignalHandler.cc
@@ -2,8 +2,12 @@
 // Author:
 //   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
 //
+// Contributor:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
 // Copyright:
 //   Thorsten Brunklaus, 2002
+//   Leif Kornstaedt, 2002
 //
 // Last Change:
 //   $Date$ by $Author$
@@ -20,163 +24,159 @@
 #include "generic/RootSet.hh"
 #include "generic/Transients.hh"
 
-typedef struct {
-  int signal;
-  bool pending;
-  word handlers;
-} SigHandler;
-
-#define SIGLAST -1
-
-static SigHandler sigHandlers[] =  {
-  { SIGALRM, false, Store::IntToWord(0) },
-  { SIGLAST, false, Store::IntToWord(0) }
-};
-
-class Entry : public Tuple {
-private:
-  enum { CLOSURE_POS, NEXT_POS, SIZE };
-public:
-  word GetClosure() {
-    return Tuple::Sel(CLOSURE_POS);
-  }
-  word GetNext() {
-    return Tuple::Sel(NEXT_POS);
-  }
-  static Entry *New(word closure, word next) {
-    Tuple *entry = Tuple::New(SIZE);
-    entry->Init(CLOSURE_POS, closure);
-    entry->Init(NEXT_POS, next);
-    return static_cast<Entry *>(entry);
-  }
-  static Entry *FromWordDirect(word entry) {
-    return static_cast<Entry *>(Tuple::FromWordDirect(entry));
-  }
-};
-
-class TimerEntry : public Tuple {
-private:
-  enum { CLOSURE_POS, NEXT_POS, TIME_POS, SIZE };
-public:
-  word GetClosure() {
-    return Tuple::Sel(CLOSURE_POS);
-  }
-  word GetNext() {
-    return Tuple::Sel(NEXT_POS);
-  }
-  u_int GetTime() {
-    return Store::DirectWordToInt(Tuple::Sel(TIME_POS));
-  }
-  void SetNext(word entry) {
-    static_cast<Block *>(this)->ReplaceArg(NEXT_POS, entry);
-  }
-  static TimerEntry *New(word closure, u_int time, word next) {
-    Tuple *entry = Tuple::New(SIZE);
-    entry->Init(CLOSURE_POS, closure);
-    entry->Init(NEXT_POS, next);
-    entry->Init(TIME_POS, Store::IntToWord(time));
-    return static_cast<TimerEntry *>(entry);
-  }
-  static TimerEntry *FromWordDirect(word entry) {
-    return static_cast<TimerEntry *>(Tuple::FromWordDirect(entry));
-  }
-};
-
 #if defined(__MINGW32__) || defined(_MSC_VER)
 #include <windows.h>
-
-class Timer {
-protected:
-  static Timer *self;
-  static u_int time;
-public:
-  HANDLE thread;
-  int wait;
-  // Timer Constructor
-  Timer(int w);
-  static void Init() {
-    time = 0;
-    self = new Timer(10);
-  }
-  // Timer Methods
-  static HANDLE GetThread() {
-    return self->thread;
-  }
-  static u_int GetTime() {
-    return time;
-  }
-  static void Update() {
-    time++;
-    StatusWord::SetStatus(Scheduler::PreemptStatus() |
-			  SignalHandler::SignalStatus());
-    sigHandlers[0].pending = true;
-  }
-};
-
-Timer *Timer::self;
-u_int Timer::time;
-
-static DWORD __stdcall TimerFunction(void *p) {
-  Timer *timer = static_cast<Timer *>(p);
-  // Make sure that this thread is not mixed with others
-  if (SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_HIGHEST) == FALSE) {
-    Error("SetThreadPriority failed");
-  }
-  while(1) {
-    Sleep(timer->wait);
-    Timer::Update();
-  }
-  delete timer;
-  ExitThread(1);
-  return 1;
-}
-
-Timer::Timer(int w) {
-  wait = w;
-  DWORD threadId;
-  thread = CreateThread(NULL, 10000, TimerFunction, this, 0, &threadId);
-  if (thread == NULL) {
-    // to be done: critical or warning
-    Error("Unable to start timer thread");
-  }
-}
 #else
 #include <sys/types.h>
 #include <time.h>
 #include <sys/time.h>
+#endif
 
+static const u_int TIME_SLICE = 10; // milliseconds
+
+word alarmHandlers; // sorted by time (ascending)
+
+struct SigHandler {
+  int signal;
+  bool pending;
+  word handlers;
+};
+
+#define SIGLAST -1
+
+static SigHandler sigHandlers[] =  {
+  { SIGLAST, false, Store::IntToWord(0) }
+};
+
+class SignalEntry: public Block {
+private:
+  static const BlockLabel SIGNAL_ENTRY_LABEL = MIN_DATA_LABEL;
+  enum { CLOSURE_POS, NEXT_POS, SIZE };
+public:
+  static SignalEntry *New(word closure, word next) {
+    Block *entry = Store::AllocBlock(SIGNAL_ENTRY_LABEL, SIZE);
+    entry->InitArg(CLOSURE_POS, closure);
+    entry->InitArg(NEXT_POS, next);
+    return static_cast<SignalEntry *>(entry);
+  }
+  static SignalEntry *FromWordDirect(word entry) {
+    Block *b = Store::DirectWordToBlock(entry);
+    Assert(b->GetLabel() == SIGNAL_ENTRY_LABEL && b->GetSize() == SIZE);
+    return static_cast<SignalEntry *>(b);
+  }
+
+  word GetClosure() {
+    return GetArg(CLOSURE_POS);
+  }
+  word GetNext() {
+    return GetArg(NEXT_POS);
+  }
+};
+
+class TimerEntry: public Block {
+private:
+  static const BlockLabel TIMER_ENTRY_LABEL = MIN_DATA_LABEL;
+  enum { TIME_POS, FUTURE_POS, NEXT_POS, SIZE };
+public:
+  static TimerEntry *New(u_int time, Future *future) {
+    Block *entry = Store::AllocBlock(TIMER_ENTRY_LABEL, SIZE);
+    entry->InitArg(TIME_POS, time);
+    entry->InitArg(FUTURE_POS, future->ToWord());
+    entry->InitArg(NEXT_POS, 0);
+    return static_cast<TimerEntry *>(entry);
+  }
+  static TimerEntry *FromWordDirect(word entry) {
+    Block *b = Store::DirectWordToBlock(entry);
+    Assert(b->GetLabel() == TIMER_ENTRY_LABEL && b->GetSize() == SIZE);
+    return static_cast<TimerEntry *>(b);
+  }
+
+  u_int GetTime() {
+    return Store::DirectWordToInt(GetArg(TIME_POS));
+  }
+  Future *GetFuture() {
+    Transient *transient = Store::DirectWordToTransient(GetArg(FUTURE_POS));
+    Assert(transient->GetLabel() == FUTURE_LABEL);
+    return static_cast<Future *>(transient);
+  }
+  TimerEntry *GetNext() {
+    word next = GetArg(NEXT_POS);
+    if (next == Store::IntToWord(0))
+      return INVALID_POINTER;
+    else
+      return FromWordDirect(next);
+  }
+  void SetNext(TimerEntry *entry) {
+    ReplaceArg(NEXT_POS, entry->ToWord());
+  }
+};
+
+#if defined(__MINGW32__) || defined(_MSC_VER)
 class Timer {
-protected:
+private:
   static u_int time;
+  static HANDLE thread;
 
-  static void SetAlarmTimer(u_int time) {
-    struct itimerval newT;
-    int sec  = time/1000;
-    int usec = (time*1000)%1000000;
-    newT.it_interval.tv_sec  = sec;
-    newT.it_interval.tv_usec = usec;
-    newT.it_value.tv_sec     = sec;
-    newT.it_value.tv_usec    = usec;
-    if (setitimer(ITIMER_REAL, &newT, NULL) < 0) {
-      // to be done: critical or warning
-      Error("setitimer failed\n");
+  static void Update() {
+    time++;
+    StatusWord::SetStatus(Scheduler::PreemptStatus() |
+			  SignalHandler::SignalStatus());
+  }
+  static DWORD __stdcall TimerFunction(void *) {
+    // Make sure that this thread is not mixed with others
+    if (SetThreadPriority(GetCurrentThread(),
+			  THREAD_PRIORITY_HIGHEST) == FALSE)
+      Error("SetThreadPriority failed");
+    while (true) {
+      Sleep(TIME_SLICE);
+      Update();
     }
   }
-  static void Update(int) {
-    time++;
-    StatusWord::SetStatus(Scheduler::PreemptStatus() | 
-			  SignalHandler::SignalStatus());
-    sigHandlers[0].pending = true;
-    SetAlarmTimer(10);
-  }
 public:
-  // Timer Static Constructor
   static void Init() {
     time = 0;
-    for (u_int i = 0; sigHandlers[i].signal != SIGLAST; i++)
-      RootSet::Add(sigHandlers[i].handlers);
-    signal(SIGALRM, Timer::Update);
-    SetAlarmTimer(10);
+    DWORD threadId;
+    thread = CreateThread(NULL, 1024, TimerFunction, NULL, 0, &threadId);
+    if (thread == NULL)
+      Error("Unable to start timer thread");
+  }
+  static u_int GetTime() {
+    return time;
+  }
+  static void Suspend() {
+    SuspendThread(thread);
+  }
+  static void Resume() {
+    ResumeThread(thread);
+  }
+};
+
+u_int Timer::time;
+HANDLE Timer::thread;
+#else
+class Timer {
+private:
+  static u_int time;
+
+  static void Update(int) {
+    time++;
+    StatusWord::SetStatus(Scheduler::PreemptStatus() |
+			  SignalHandler::SignalStatus());
+  }
+public:
+  static void Init() {
+    time = 0;
+    signal(SIGALRM, Update);
+
+    struct itimerval value;
+    int sec  = TIME_SLICE / 1000;
+    int usec = (TIME_SLICE % 1000) * 1000;
+    value.it_interval.tv_sec  = sec;
+    value.it_interval.tv_usec = usec;
+    value.it_value.tv_sec     = sec;
+    value.it_value.tv_usec    = usec;
+    if (setitimer(ITIMER_REAL, &value, NULL) < 0)
+      Error("setitimer failed");
   }
   static u_int GetTime() {
     return time;
@@ -186,52 +186,9 @@ public:
 u_int Timer::time;
 #endif
 
-//
-// SignalHandler Methods
-//
-void SignalHandler::PushCall(word closure, int signal) {
-  Thread *thread = Scheduler::NewThread(closure, 1, Store::IntToWord(signal));
-  Scheduler::ScheduleThread(thread);
-}
-
-static Future *WordToFuture(word future) {
-  Transient *transient = Store::WordToTransient(future);
-  Assert(transient != INVALID_POINTER &&
-	 transient->GetLabel() == FUTURE_LABEL);
-  return static_cast<Future *>(transient);
-}
-
-void SignalHandler::CheckTimerEvents() {
-  word handlers = sigHandlers[0].handlers;
-  TimerEntry *prev = NULL;
-  sigHandlers[0].pending = false;
-  while (handlers != Store::IntToWord(0)) {
-    TimerEntry *entry = TimerEntry::FromWordDirect(handlers);
-    handlers = entry->GetNext();
-    // We might have missed some ticks
-    if (entry->GetTime() <= Timer::GetTime()) {
-      Future *future = WordToFuture(entry->GetClosure());
-      future->ScheduleWaitingThreads();
-      future->Become(REF_LABEL, Store::IntToWord(0));
-      if (prev == NULL)
-	sigHandlers[0].handlers = handlers;
-      else
-	prev->SetNext(handlers);
-    }
-    else
-      prev = entry;
-  }
-}
-
-void SignalHandler::Init() {
-  for (u_int i = 0; sigHandlers[i].signal != SIGLAST; i++)
-    RootSet::Add(sigHandlers[i].handlers);
-  Timer::Init();
-}
-
-void SignalHandler::BlockSignals() {
+static void BlockSignals() {
 #if defined(__MINGW32__) || defined(_MSC_VER)
-  SuspendThread(Timer::GetThread());
+  Timer::Suspend();
 #else
   sigset_t set;
   sigfillset(&set);
@@ -243,9 +200,9 @@ void SignalHandler::BlockSignals() {
 #endif
 }
 
-void SignalHandler::UnblockSignals() {
+static void UnblockSignals() {
 #if defined(__MINGW32__) || defined(_MSC_VER)
-  ResumeThread(Timer::GetThread());
+  Timer::Resume();
 #else
   sigset_t set;
   sigemptyset(&set);
@@ -253,50 +210,89 @@ void SignalHandler::UnblockSignals() {
 #endif
 }
 
-static int FindSignal(int signal) {
-  u_int i = 0;
-  while (sigHandlers[i].signal != signal) {
-    if (sigHandlers[i].signal == SIGLAST) {
-      Error("illegal signal");
-    }
-    i++;
-  }
-  return i;
+//
+// SignalHandler Methods
+//
+void SignalHandler::Init() {
+  alarmHandlers = Store::IntToWord(0);
+  RootSet::Add(alarmHandlers);
+  for (u_int i = 0; sigHandlers[i].signal != SIGLAST; i++)
+    RootSet::Add(sigHandlers[i].handlers);
+  Timer::Init();
 }
 
-void
-SignalHandler::Register(int signal, word closure, u_int delay = 0) {
+static int FindSignal(int signal) {
+  for (u_int i = 0; sigHandlers[i].signal != SIGLAST; i++)
+    if (sigHandlers[i].signal == signal)
+      return i;
+  Error("illegal signal");
+}
+
+void SignalHandler::RegisterSignal(int signal, word closure) {
   BlockSignals();
   u_int i = FindSignal(signal);
-  word entry;
-  if (signal == SIGALRM) {
-    u_int tickTime = Timer::GetTime() + (delay / 10);
-    entry =
-      TimerEntry::New(closure, tickTime, sigHandlers[i].handlers)->ToWord();
-  }
-  else
-    entry = Entry::New(closure, sigHandlers[i].handlers)->ToWord();
-  sigHandlers[i].handlers = entry;
+  sigHandlers[i].handlers =
+    SignalEntry::New(closure, sigHandlers[i].handlers)->ToWord();
   UnblockSignals();
 }
 
-bool SignalHandler::PendingTimerEvents() {
-  return (sigHandlers[0].handlers != Store::IntToWord(0));
+Future *SignalHandler::RegisterAlarm(u_int milliseconds) {
+  BlockSignals();
+  u_int time = Timer::GetTime() + (milliseconds + TIME_SLICE - 1) / TIME_SLICE;
+  Future *future = Future::New();
+  TimerEntry *newEntry = TimerEntry::New(time, future);
+  if (alarmHandlers == Store::IntToWord(0)) // list was empty
+    alarmHandlers = newEntry->ToWord();
+  else { // insert into list, sorted by time (ascending)
+    TimerEntry *entry = TimerEntry::FromWordDirect(alarmHandlers);
+    TimerEntry *prev = INVALID_POINTER;
+    while (entry->GetTime() <= time) { // skip this entry?
+      prev = entry;
+      entry = entry->GetNext();
+      if (entry == INVALID_POINTER) // reached end of list
+	goto done;
+    }
+    newEntry->SetNext(entry);
+  done:
+    if (prev == INVALID_POINTER)
+      alarmHandlers = newEntry->ToWord();
+    else
+      prev->SetNext(newEntry);
+  }
+  UnblockSignals();
+  return future;
+}
+
+static void CheckAlarms() {
+  if (alarmHandlers == Store::IntToWord(0)) // no alarms
+    return;
+  TimerEntry *entry = TimerEntry::FromWordDirect(alarmHandlers);
+  while (entry->GetTime() <= Timer::GetTime()) { // triggered?
+    Future *future = entry->GetFuture();
+    future->ScheduleWaitingThreads();
+    future->Become(REF_LABEL, Store::IntToWord(0)); // unit
+    entry = entry->GetNext();
+    if (entry == INVALID_POINTER) { // all alarms have been triggered
+      alarmHandlers = Store::IntToWord(0);
+      return;
+    }
+  }
+  alarmHandlers = entry->ToWord();
 }
 
 void SignalHandler::HandlePendingSignals() {
   BlockSignals();
-  CheckTimerEvents();
-  for (u_int i = 1; sigHandlers[i].signal != SIGLAST; i++)
+  CheckAlarms();
+  for (u_int i = 0; sigHandlers[i].signal != SIGLAST; i++)
     if (sigHandlers[i].pending) {
       sigHandlers[i].pending = false;
       word handlers = sigHandlers[i].handlers;
       while (handlers != Store::IntToWord(0)) {
-	Entry *entry = Entry::FromWordDirect(handlers);
-	handlers     = entry->GetNext();
+	SignalEntry *entry = SignalEntry::FromWordDirect(handlers);
 	word closure = entry->GetClosure();
-	// to be done: Use thread instead?
-	SignalHandler::PushCall(closure, sigHandlers[i].signal);
+	Scheduler::NewThread(closure, 1,
+			     Store::IntToWord(sigHandlers[i].signal));
+	handlers = entry->GetNext();
       }
       sigHandlers[i].handlers = Store::IntToWord(0);
     }
