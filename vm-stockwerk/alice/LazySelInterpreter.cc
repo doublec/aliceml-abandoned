872df23057a6f1a6b62331d3b1bc9e9656301f98
kornstae <none@none>
2002-08-12 17:08:20 +0100

    Creating a single byneed to handle all lazy poly sels on a record    was a nice idea, but doesn't work:    this was the reason that the concurrent fib example (written using spawn)    did not work.    Now one byneed is created per item again.

--- vm-stockwerk/alice/LazySelInterpreter.cc
+++ vm-stockwerk/alice/LazySelInterpreter.cc
@@ -18,42 +18,37 @@
 
 #include <cstdio>
 #include "generic/StackFrame.hh"
-#include "generic/Transients.hh"
 #include "generic/Scheduler.hh"
+#include "alice/Data.hh"
 #include "alice/LazySelInterpreter.hh"
 
 // LazySel Frame
 class LazySelFrame: private StackFrame {
 private:
-  enum { RECORD_POS, LABELS_POS, BYNEEDS_POS, SIZE };
+  enum { RECORD_POS, LABEL_POS, SIZE };
 public:
   using Block::ToWord;
   using StackFrame::GetInterpreter;
-  // LazySelFrame Accessors
-  word GetRecord() {
-    return GetArg(RECORD_POS);
-  }
-  Vector *GetLabels() {
-    return Vector::FromWordDirect(GetArg(LABELS_POS));
-  }
-  Vector *GetByneeds() {
-    return Vector::FromWordDirect(GetArg(BYNEEDS_POS));
-  }
-  // LazySelFrame Constructor
+
   static LazySelFrame *New(Interpreter *interpreter, word record,
-			   Vector *labels, Vector *byneeds) {
+			   UniqueString *label) {
     StackFrame *frame =
       StackFrame::New(LAZY_SELECTION_FRAME, interpreter, SIZE);
     frame->InitArg(RECORD_POS, record);
-    frame->InitArg(LABELS_POS, labels->ToWord());
-    frame->InitArg(BYNEEDS_POS, byneeds->ToWord());
+    frame->InitArg(LABEL_POS, label->ToWord());
     return static_cast<LazySelFrame *>(frame);
   }
-  // LazySelFrame Untagging
   static LazySelFrame *FromWordDirect(word frame) {
     StackFrame *p = StackFrame::FromWordDirect(frame);
     Assert(p->GetLabel() == LAZY_SELECTION_FRAME);
     return static_cast<LazySelFrame *>(p);
+  }
+
+  word GetRecord() {
+    return GetArg(RECORD_POS);
+  }
+  UniqueString *GetLabel() {
+    return UniqueString::FromWordDirect(GetArg(LABEL_POS));
   }
 };
 
@@ -65,8 +60,7 @@ LazySelInterpreter *LazySelInterpreter::self;
 void LazySelInterpreter::PushCall(Closure *closure0) {
   LazySelClosure *closure = static_cast<LazySelClosure *>(closure0);
   LazySelFrame *frame =
-    LazySelFrame::New(self, closure->GetRecord(),
-		      closure->GetLabels(), closure->GetByneeds());
+    LazySelFrame::New(self, closure->GetRecord(), closure->GetLabel());
   Scheduler::PushFrame(frame->ToWord());
 }
 
@@ -75,27 +69,10 @@ Interpreter::Result LazySelInterpreter::Run() {
   word wRecord = frame->GetRecord();
   Transient *transient = Store::WordToTransient(wRecord);
   if (transient == INVALID_POINTER) { // is determined
-    Record *record = Record::FromWord(wRecord);
-    Vector *labels = frame->GetLabels();
-    Vector *byneeds = frame->GetByneeds();
-    Assert(labels->GetLength() == byneeds->GetLength());
-    word result = 0;
-    for (u_int i = labels->GetLength(); i--; ) {
-      Transient *transient = Store::DirectWordToTransient(byneeds->Sub(i));
-      UniqueString *label = UniqueString::FromWordDirect(labels->Sub(i));
-      word value = record->PolySel(label);
-      if (transient->GetLabel() == FUTURE_LABEL) {
-	Assert(result == 0);
-	result = value;
-      } else {
-	Assert(transient->GetLabel() == BYNEED_LABEL);
-	transient->Become(REF_LABEL, value);
-      }
-    }
-    Assert(result != 0);
     Scheduler::PopFrame();
     Scheduler::nArgs = Scheduler::ONE_ARG;
-    Scheduler::currentArgs[0] = result;
+    Scheduler::currentArgs[0] =
+      Record::FromWord(wRecord)->PolySel(frame->GetLabel());
     return Interpreter::CONTINUE;
   } else { // need to request
     Scheduler::currentData = wRecord;
@@ -109,11 +86,6 @@ const char *LazySelInterpreter::Identify() {
 
 void LazySelInterpreter::DumpFrame(word frameWord) {
   LazySelFrame *frame = LazySelFrame::FromWordDirect(frameWord);
-  std::fprintf(stderr, "Select");
-  Vector *labels = frame->GetLabels();
-  for (u_int i = labels->GetLength(); i--; ) {
-    UniqueString *label = UniqueString::FromWordDirect(labels->Sub(i));
-    std::fprintf(stderr, " %s", label->ToString()->ExportC());
-  }
-  std::fprintf(stderr, "\n");
+  std::fprintf(stderr, "Select %s\n",
+	       frame->GetLabel()->ToString()->ExportC());
 }
