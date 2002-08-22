8f732810ff0ac2c6b9b29b2e9a258a278dd4ddcc
kornstae <none@none>
2002-08-22 12:27:16 +0100

    Include file for use by foreign components.

---
+++ vm-stockwerk/Stockwerk.hh
@@ -0,0 +1,61 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#ifndef __STOW_HH__
+#define __STOW_HH__
+
+#define STOCKWERK_FOREIGN
+
+#include "store/Store.hh"
+#include "adt/HashTable.hh"
+#include "adt/Queue.hh"
+#include "adt/Stack.hh"
+#include "generic/FinalizationSet.hh"
+#include "generic/Transform.hh"
+#include "generic/ConcreteRepresentationHandler.hh"
+#include "generic/ConcreteRepresentation.hh"
+#include "generic/ConcreteCode.hh"
+#include "generic/Closure.hh"
+#include "generic/Thread.hh"
+#include "generic/ThreadQueue.hh"
+#include "generic/Tuple.hh"
+#include "generic/String.hh"
+#include "generic/StackFrame.hh"
+#include "generic/Backtrace.hh"
+#include "generic/Pickle.hh"
+#include "generic/Debug.hh"
+#include "generic/RootSet.hh"
+#include "generic/UniqueString.hh"
+#include "generic/Properties.hh"
+#include "generic/TaskStack.hh"
+#include "generic/IOHandler.hh"
+#include "generic/SignalHandler.hh"
+#include "generic/Scheduler.hh"
+#include "generic/Transients.hh"
+#include "generic/Interpreter.hh"
+#include "generic/Primitive.hh"
+#include "generic/PushCallInterpreter.hh"
+#include "generic/ByneedInterpreter.hh"
+#include "generic/Unpickler.hh"
+#include "generic/Pickler.hh"
+#include "generic/Profiler.hh"
+#include "alice/Guid.hh"
+#include "alice/Data.hh"
+#include "alice/AbstractCode.hh"
+#include "alice/AbstractCodeInterpreter.hh"
+#include "alice/AliceConcreteCode.hh"
+#include "alice/NativeCodeInterpreter.hh"
+#include "alice/NativeConcreteCode.hh"
+#include "alice/PrimitiveTable.hh"
+#include "alice/AliceLanguageLayer.hh"
+
+#endif
