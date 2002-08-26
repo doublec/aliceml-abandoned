c126ac4190a7028f8a7ae63d18c747cb395395b6
kornstae <none@none>
2002-08-26 11:18:05 +0100

    Separated main from StockwerkMain, which will be the entry point into stow.dll.

--- generic/Properties.cc
+++ generic/Properties.cc
@@ -21,30 +21,50 @@
 #include "generic/Properties.hh"
 #include "generic/RootSet.hh"
 #include "generic/String.hh"
+#include "alice/Data.hh" //--**
 
 word Properties::aliceHome;
 word Properties::rootUrl;
 word Properties::commandLineArguments;
 word Properties::atExn;
 
-void Properties::Init() {
+void Properties::Init(char *home, u_int argc, char *argv[]) {
   RootSet::Add(aliceHome);
   RootSet::Add(rootUrl);
   RootSet::Add(commandLineArguments);
   RootSet::Add(atExn);
 
-  char *home = std::getenv("STOCKHOME");
-  if (home == NULL) {
-    Error("could not determine installation directory");
-  }
+  // Initialize aliceHome, making sure it ends in a trailing slash:
   u_int n = std::strlen(home);
-  String *homeString = String::New(n + 1);
-  u_char *p = homeString->GetValue();
-  std::memcpy(p, home, n);
-  p[n] = '/';
-  aliceHome = homeString->ToWord();
+  if (n != 0 && home[n - 1] == '/') {
+    aliceHome = String::New(home, n)->ToWord();
+  } else {
+    String *homeString = String::New(n + 1);
+    u_char *p = homeString->GetValue();
+    std::memcpy(p, home, n);
+    p[n] = '/';
+    aliceHome = homeString->ToWord();
+  }
 
-  rootUrl = Store::IntToWord(0);
-  commandLineArguments = Store::IntToWord(0);
+  // Initialize rootUrl:
+  if (argc < 2) {
+    rootUrl = Store::IntToWord(0);
+  } else {
+    Properties::rootUrl = String::New(argv[1])->ToWord();
+    argv++; argc--;
+  }
+
+  // Initialize commandLineArguments:
+  word list = Store::IntToWord(1); // nil
+  argv++; argc--;
+  for (u_int i = argc; i--; ) {
+    TagVal *cons = TagVal::New(0, 2); // ::
+    cons->Init(0, String::New(argv[i])->ToWord());
+    cons->Init(1, list);
+    list = cons->ToWord();
+  }
+  Properties::commandLineArguments = list;
+
+  // Initialize atExn:
   atExn = Store::IntToWord(0);
 }
