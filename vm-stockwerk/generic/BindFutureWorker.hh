8afcf7231ee4365118c0df8643386e092cddcb4a
kornstae <none@none>
2002-08-27 15:38:25 +0100

    Try to better separate Worker from closure handler    (which is still a misnomer since it's called Interpreter).

--- vm-stockwerk/generic/BindFutureWorker.hh
+++ vm-stockwerk/generic/BindFutureWorker.hh
@@ -1,37 +1,39 @@
 //
 // Authors:
 //   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
 //
 // Copyright:
 //   Thorsten Brunklaus, 2002
+//   Leif Kornstaedt, 2002
 //
 // Last Change:
 //   $Date$ by $Author$
 //   $Revision$
 //
 
-#ifndef __GENERIC__BYNEED_INTERPRETER_HH__
-#define __GENERIC__BYNEED_INTERPRETER_HH__
+#ifndef __GENERIC__BIND_FUTURE_WORKER_HH__
+#define __GENERIC__BIND_FUTURE_WORKER_HH__
 
 #if defined(INTERFACE)
-#pragma interface "generic/ByneedInterpreter.hh"
+#pragma interface "generic/BindFutureWorker.hh"
 #endif
 
-#include "generic/Interpreter.hh"
+#include "generic/Worker.hh"
 
 class Thread;
 class Transient;
 
-class DllExport ByneedInterpreter: public Interpreter {
+class DllExport BindFutureWorker: public Worker {
 private:
-  // ByneedInterpreter Constructor
-  ByneedInterpreter(): Interpreter() {}
+  // BindFutureWorker Constructor
+  BindFutureWorker(): Worker() {}
 public:
-  // Exported ByneedInterpreter Instance
-  static ByneedInterpreter *self;
-  // ByneedInterpreter Static Constructor
+  // Exported BindFutureWorker Instance
+  static BindFutureWorker *self;
+  // BindFutureWorker Static Constructor
   static void Init() {
-    self = new ByneedInterpreter();
+    self = new BindFutureWorker();
   }
   // Frame Handling
   static void PushFrame(Thread *thread, Transient *future);
