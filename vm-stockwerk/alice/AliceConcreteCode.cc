3e1255bb798aac0b89c050eb70b1cd6c7592065e
kornstae <none@none>
2002-08-27 15:38:24 +0100

    Try to better separate Worker from closure handler    (which is still a misnomer since it's called Interpreter).

--- vm-stockwerk/alice/AliceConcreteCode.cc
+++ vm-stockwerk/alice/AliceConcreteCode.cc
@@ -36,10 +36,10 @@ void AliceConcreteCode::Disassemble(std::FILE *file) {
   Transform *transform = Transform::FromWordDirect(Get(TRANSFORM_POS));
   TagVal *abstractCode = TagVal::FromWordDirect(transform->GetArgument());
   Tuple *coord = Tuple::FromWordDirect(abstractCode->Sel(0));
-  fprintf(file, "Disassembling function at %s:%d.%d\n\n",
-	  String::FromWordDirect(coord->Sel(0))->ExportC(),
-	  Store::DirectWordToInt(coord->Sel(1)),
-	  Store::DirectWordToInt(coord->Sel(2)));
+  std::fprintf(file, "Disassembling function at %s:%d.%d\n\n",
+	       String::FromWordDirect(coord->Sel(0))->ExportC(),
+	       Store::DirectWordToInt(coord->Sel(1)),
+	       Store::DirectWordToInt(coord->Sel(2)));
   TagVal *pc = TagVal::FromWordDirect(abstractCode->Sel(4));
   AbstractCode::Disassemble(file, pc);
 }
