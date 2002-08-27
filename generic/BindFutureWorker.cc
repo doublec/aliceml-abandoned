3d40ffb8622c32275b0e7642ecd406a3d34317e6
kornstae <none@none>
2002-08-27 15:38:25 +0100

    Try to better separate Worker from closure handler    (which is still a misnomer since it's called Interpreter).

--- generic/BindFutureWorker.cc
+++ generic/BindFutureWorker.cc
@@ -13,36 +13,36 @@
 //
 
 #if defined(INTERFACE)
-#pragma implementation "generic/ByneedInterpreter.hh"
+#pragma implementation "generic/BindFutureWorker.hh"
 #endif
 
 #include <cstdio>
 #include "generic/Backtrace.hh"
 #include "generic/Scheduler.hh"
 #include "generic/Transients.hh"
-#include "generic/ByneedInterpreter.hh"
+#include "generic/BindFutureWorker.hh"
 
-// ByneedFrame
-class ByneedFrame: private StackFrame {
+// BindFutureFrame
+class BindFutureFrame: private StackFrame {
 private:
   enum { FUTURE_POS, SIZE };
 public:
   using Block::ToWord;
 
-  // ByneedFrame Constructor
-  static ByneedFrame *New(Interpreter *interpreter, Transient *future) {
-    StackFrame *frame = StackFrame::New(BYNEED_FRAME, interpreter, SIZE);
+  // BindFutureFrame Constructor
+  static BindFutureFrame *New(Worker *worker, Transient *future) {
+    StackFrame *frame = StackFrame::New(BYNEED_FRAME, worker, SIZE);
     frame->InitArg(FUTURE_POS, future->ToWord());
-    return static_cast<ByneedFrame *>(frame);
+    return static_cast<BindFutureFrame *>(frame);
   }
-  // ByneedFrame Untagging
-  static ByneedFrame *FromWordDirect(word frame) {
+  // BindFutureFrame Untagging
+  static BindFutureFrame *FromWordDirect(word frame) {
     StackFrame *p = StackFrame::FromWordDirect(frame);
     Assert(p->GetLabel() == BYNEED_FRAME);
-    return static_cast<ByneedFrame *>(p);
+    return static_cast<BindFutureFrame *>(p);
   }
 
-  // ByneedFrame Accessors
+  // BindFutureFrame Accessors
   Future *GetFuture() {
     Transient *transient =
       Store::WordToTransient(StackFrame::GetArg(FUTURE_POS));
@@ -53,52 +53,52 @@ public:
 };
 
 //
-// ByneedInterpreter Functions
+// BindFutureWorker Functions
 //
-ByneedInterpreter *ByneedInterpreter::self;
+BindFutureWorker *BindFutureWorker::self;
 
-void ByneedInterpreter::PushFrame(Thread *thread, Transient *future) {
-  thread->PushFrame(ByneedFrame::New(self, future)->ToWord());
+void BindFutureWorker::PushFrame(Thread *thread, Transient *future) {
+  thread->PushFrame(BindFutureFrame::New(self, future)->ToWord());
 }
 
 static inline bool IsCyclic(word x, Future *future) {
   return static_cast<Future *>(Store::WordToTransient(x)) == future;
 }
 
-Interpreter::Result ByneedInterpreter::Run() {
-  ByneedFrame *frame =
-    ByneedFrame::FromWordDirect(Scheduler::GetAndPopFrame());
+Worker::Result BindFutureWorker::Run() {
+  BindFutureFrame *frame =
+    BindFutureFrame::FromWordDirect(Scheduler::GetAndPopFrame());
   Future *future = frame->GetFuture();
   future->ScheduleWaitingThreads();
-  Interpreter::Construct();
+  Construct();
   word arg = Scheduler::currentArgs[0];
   if (IsCyclic(arg, future)) { // cancel future with Cyclic exception
     future->Become(CANCELLED_LABEL, Hole::cyclicExn);
     Scheduler::currentData = Hole::cyclicExn;
     Scheduler::currentBacktrace = Backtrace::New(frame->ToWord());
-    return Interpreter::RAISE;
+    return Worker::RAISE;
   } else { // actually bind the future
     future->Become(REF_LABEL, arg);
     //Scheduler::nArgs = Scheduler::ONE_ARG;
     //Scheduler::currentArgs[0] = arg;
-    return Interpreter::CONTINUE;
+    return Worker::CONTINUE;
   }
 }
 
-Interpreter::Result ByneedInterpreter::Handle() {
+Worker::Result BindFutureWorker::Handle() {
   Future *future =
-    ByneedFrame::FromWordDirect(Scheduler::GetAndPopFrame())->GetFuture();
+    BindFutureFrame::FromWordDirect(Scheduler::GetAndPopFrame())->GetFuture();
   future->ScheduleWaitingThreads();
   future->Become(CANCELLED_LABEL, Scheduler::currentData);
   Scheduler::nArgs = Scheduler::ONE_ARG;
   Scheduler::currentArgs[0] = future->ToWord();
-  return Interpreter::CONTINUE;
+  return Worker::CONTINUE;
 }
 
-const char *ByneedInterpreter::Identify() {
-  return "ByneedInterpreter";
+const char *BindFutureWorker::Identify() {
+  return "BindFutureWorker";
 }
 
-void ByneedInterpreter::DumpFrame(word) {
-  std::fprintf(stderr, "Byneed\n");
+void BindFutureWorker::DumpFrame(word) {
+  std::fprintf(stderr, "Bind future\n");
 }
