bf4e3b5142a9908246798d29850e72b01bb2b93e
kornstae <none@none>
2002-08-31 15:47:02 +0100

    Works somewhat better with spaces in pathnames; Fixed test for zlib.

--- install-stockwerk.sh
+++ install-stockwerk.sh
@@ -50,13 +50,13 @@ esac
 SUPPORTDIR=`cd support && pwd`
 if [ "$LIGHTNING" -ne 0 ]
 then
-    if [ ! -f $SUPPORTDIR/install/$SUPPORTPLATFORM/include/lightning.h ]
+    if [ ! -f "$SUPPORTDIR/install/$SUPPORTPLATFORM/include/lightning.h" ]
     then
-	mkdir -p $SUPPORTDIR/build/$SUPPORTPLATFORM/lightning 2>/dev/null
+	mkdir -p "$SUPPORTDIR/build/$SUPPORTPLATFORM/lightning" 2>/dev/null
 	(
-	    cd $SUPPORTDIR/build/$SUPPORTPLATFORM/lightning &&
-	    CC=$CC $SUPPORTDIR/lightning/configure \
-		--prefix=$SUPPORTDIR/install/$SUPPORTPLATFORM &&
+	    cd "$SUPPORTDIR/build/$SUPPORTPLATFORM/lightning" &&
+	    CC=$CC "$SUPPORTDIR/lightning/configure" \
+		--prefix="$SUPPORTDIR/install/$SUPPORTPLATFORM" &&
 	    make all install
 	) || exit 1
     fi
@@ -65,10 +65,10 @@ fi
 ##
 ## Build Support Libraries: zlib
 ##
-if [ ! -f $SUPPORTDIR/install/$SUPPORTPLATFORM/include/xyz.h ]
+if [ ! -f "$SUPPORTDIR/install/$SUPPORTPLATFORM/include/zlib.h" ]
 then
-    cd $SUPPORTDIR/zlib &&
-    CC=$CC ./configure --prefix=$SUPPORTDIR/install/$SUPPORTPLATFORM &&
+    cd "$SUPPORTDIR/zlib" &&
+    CC=$CC ./configure --prefix="$SUPPORTDIR/install/$SUPPORTPLATFORM" &&
     make all install distclean || exit 1
 fi
 
