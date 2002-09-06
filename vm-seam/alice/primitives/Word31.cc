d03798d5c8542e5f538185fa2360e95ca4f9a19f
kornstae <none@none>
2002-09-06 16:02:50 +0100

    Specialized Word primitives for word sizes 8 and 31.

---
+++ vm-seam/alice/primitives/Word31.cc
@@ -0,0 +1,16 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#define WORD_PRECISION 31
+#define WordN Word31
+
+#include "Word.icc"
