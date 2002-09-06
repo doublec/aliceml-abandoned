2dcd5950ab7c297140125bf72fdb006e22f1b0bb
kornstae <none@none>
2002-09-06 16:07:07 +0100

    Primitives for Word8Vectors.

---
+++ vm-stockwerk/alice/primitives/Word8Vector.cc
@@ -0,0 +1,16 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#define XVector Word8Vector
+#define YVector CharVector
+
+#include "Vector.icc"
