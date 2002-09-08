93736db7463441983ffcdb3ee48488de46ab347c
kornstae <none@none>
2002-09-08 18:34:16 +0100

    Added Word8Array primitives.

---
+++ vm-stockwerk/alice/primitives/Word8Array.cc
@@ -0,0 +1,16 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#define XArray Word8Array
+#define REQUEST_ELEMS_INT 1
+
+#include "Array.icc"
