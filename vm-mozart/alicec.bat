e9db651124ea2ba5e1492ee4549f1745d6e25a17
kornstae <none@none>
2002-09-11 12:48:01 +0100

    Made some batch files actually work when STOCKHOME/bin is not in the PATH.

--- vm-mozart/alicec.bat
+++ vm-mozart/alicec.bat
@@ -1,2 +1,2 @@
 @echo off
-alicerun x-alice:/compiler/CompilerMain %1 %2 %3 %4 %5 %6 %7 %8 %9
+%~dp0\alicerun x-alice:/compiler/CompilerMain %1 %2 %3 %4 %5 %6 %7 %8 %9
--- vm-mozart/alicelink.bat
+++ vm-mozart/alicelink.bat
@@ -1,2 +1,2 @@
 @echo off
-alicerun x-alice:/linker/Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+%~dp0\alicerun x-alice:/linker/Main %1 %2 %3 %4 %5 %6 %7 %8 %9
