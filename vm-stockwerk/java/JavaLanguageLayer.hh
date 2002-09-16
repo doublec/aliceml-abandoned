eb61d61f540ee36bc13052253fbfc32749c7e863
kornstae <none@none>
2002-09-16 13:35:40 +0100

    Language layer initialization code needed for workers.

---
+++ vm-stockwerk/java/JavaLanguageLayer.hh
@@ -0,0 +1,25 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#ifndef __JAVA__JAVA_LANGUAGE_LAYER_HH__
+#define __JAVA__JAVA_LANGUAGE_LAYER_HH__
+
+#if defined(INTERFACE)
+#pragma interface "java/JavaLanguageLayer.hh"
+#endif
+
+class DllExport JavaLanguageLayer {
+public:
+  static void Init();
+};
+
+#endif
