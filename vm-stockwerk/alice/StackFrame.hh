1a9269f6aba0ec75c3a350550b7ffe52933ee788
kornstae <none@none>
2002-09-16 12:22:21 +0100

    Eps.

--- vm-stockwerk/alice/StackFrame.hh
+++ vm-stockwerk/alice/StackFrame.hh
@@ -19,30 +19,32 @@
 #include "generic/StackFrame.hh"
 
 static const FrameLabel ABSTRACT_CODE_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 1);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 1);
 static const FrameLabel ABSTRACT_CODE_HANDLER_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 2);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 2);
 static const FrameLabel LAZY_SELECTION_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 3);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 3);
 static const FrameLabel VECTOR_TABULATE_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 4);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 4);
 static const FrameLabel RAISE_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 5);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 5);
 static const FrameLabel CELLMAP_INSERT_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 6);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 6);
 static const FrameLabel CELLMAP_ITERATOR_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 7);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 7);
 static const FrameLabel CELLMAP_FIND_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 8);
-  // AliceNativeCode Frames
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 8);
+
+// AliceNativeCode Frames
 static const FrameLabel NATIVE_CODE_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 9);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 9);
 static const FrameLabel NATIVE_CODE_HANDLER_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 10);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 10);
 static const FrameLabel LAZY_COMPILE_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 11);
-  // Alice Async IO Frames
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 11);
+
+// Alice Async IO Frames
 static const FrameLabel FD_INPUT_FRAME =
-(FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 12);
+  (FrameLabel) (MIN_LANGUAGE_LAYER_FRAME + 12);
 
 #endif
