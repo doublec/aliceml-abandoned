1448917dd87cf6efe76ec82683b9ae7edcde4335
kornstae <none@none>
2002-09-16 15:36:59 +0100

    FFI.

---
+++ vm-stockwerk/Java.hh
@@ -0,0 +1,25 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#ifndef __JAVA_HH__
+#define __JAVA_HH__
+
+#include "Stockwerk.hh"
+#include "java/Data.hh"
+#include "java/StackFrame.hh"
+#include "java/ClassInfo.hh"
+#include "java/ClassFile.hh"
+#include "java/ClassLoader.hh"
+#include "java/ByteCodeInterpreter.hh"
+#include "java/JavaLanguageLayer.hh"
+
+#endif
