064608260a3873313d5bbfeff7f08bbebabc9240
bruni <none@none>
2002-09-16 12:12:40 +0100

    Introduced java/StackFrame.hh

---
+++ vm-stockwerk/java/StackFrame.hh
@@ -0,0 +1,24 @@
+//
+// Authors:
+//   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
+//
+// Copyright:
+//   Thorsten Brunklaus, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#ifndef __ALICE__STACK_FRAME_HH__
+#define __ALICE__STACK_FRAME_HH__
+
+#if defined(INTERFACE)
+#pragma interface "alice/StackFrame.hh"
+#endif
+#include "generic/StackFrame.hh"
+
+static const FrameLabel JAVA_BYTE_CODE_FRAME =
+(FrameLabel) MIN_LANGUAGE_LAYER_FRAME + 1;
+
+#endif
