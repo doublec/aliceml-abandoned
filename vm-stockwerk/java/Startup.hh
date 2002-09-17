72e1cabc907633145c5b79f161ee0b3a47d13166
kornstae <none@none>
2002-09-17 14:59:06 +0100

    Added Java virtual machine startup code.

---
+++ vm-stockwerk/java/Startup.hh
@@ -0,0 +1,22 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#ifndef __JAVA__STARTUP_HH__
+#define __JAVA__STARTUP_HH__
+
+#if defined(INTERFACE)
+#pragma interface "java/Startup.hh"
+#endif
+
+void Startup();
+
+#endif
