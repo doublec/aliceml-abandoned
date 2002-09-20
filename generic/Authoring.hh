d4390cd8c0967ca2927ee60876bd2787692901ad
kornstae <none@none>
2002-09-20 15:51:50 +0100

    Added DECLARE_FLOAT.

--- generic/Authoring.hh
+++ generic/Authoring.hh
@@ -116,7 +116,8 @@
 #define DECLARE_CLOSURE(closure, x) DECLARE_BLOCKTYPE(Closure, closure, x)
 #define DECLARE_TUPLE(tuple, x) DECLARE_BLOCKTYPE(Tuple, tuple, x)
 #define DECLARE_STRING(string, x) DECLARE_BLOCKTYPE(String, string, x)
-#define DECLARE_DOUBLE(double, x) DECLARE_BLOCKTYPE(Double, double, x)
+#define DECLARE_FLOAT(f, x) DECLARE_BLOCKTYPE(Float, f, x)
+#define DECLARE_DOUBLE(d, x) DECLARE_BLOCKTYPE(Double, d, x)
 #define DECLARE_UNIQUE_STRING(uniqueString, x) \
   DECLARE_BLOCKTYPE(UniqueString, uniqueString, x)
 #define DECLARE_HASH_TABLE(hashtable, x) \
