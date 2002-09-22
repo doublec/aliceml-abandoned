fcc2c2bd7bdfab9d324f565d793aae66244cabda
kornstae <none@none>
2002-09-22 21:11:58 +0100

    Added forName0.

--- vm-stockwerk/java/java/lang/Class.cc
+++ vm-stockwerk/java/java/lang/Class.cc
@@ -10,6 +10,7 @@
 //   $Revision$
 //
 
+#include "java/ClassLoader.hh"
 #include "java/Authoring.hh"
 
 DEFINE0(registerNatives) {
@@ -17,7 +18,32 @@ DEFINE0(registerNatives) {
 } END
 
 DEFINE1(getPrimitiveClass) {
+  DECLARE_JAVA_STRING(name, x0);
   RETURN(null); //--**
+} END
+
+DEFINE3(forName0) {
+  DECLARE_JAVA_STRING(name, x0);
+  DECLARE_BOOL(initialize, x1);
+  DECLARE_CLASS_LOADER(classLoader, x2);
+  u_int length = name->GetLength();
+  BaseArray *a = BaseArray::New(PrimitiveType::Char, length);
+  for (u_int i = length; i--; ) {
+    u_wchar c = name->CharAt(i);
+    if (c == '.') c = '/';
+    a->StoreChar(i, c);
+  }
+  JavaString *internalName = JavaString::New(a, 0, length);
+  if (classLoader == INVALID_POINTER)
+    classLoader = ClassLoader::GetBootstrapClassLoader();
+  word wClass = classLoader->ResolveClass(internalName);
+  Class *theClass = Class::FromWord(wClass);
+  if (theClass == INVALID_POINTER) REQUEST(wClass);
+  if (!theClass->IsInitialized() && initialize) {
+    Scheduler::PushFrameNoCheck(prim_self);
+    return theClass->RunInitializer();
+  }
+  RETURN(theClass->ToWord());
 } END
 
 void NativeMethodTable::java_lang_Class(JavaString *className) {
@@ -25,4 +51,7 @@ void NativeMethodTable::java_lang_Class(JavaString *className) {
   Register(className, "getPrimitiveClass",
 	   "(Ljava/lang/String;)Ljava/lang/Class;",
 	   getPrimitiveClass, 1, false);
+  Register(className, "forName0",
+	   "(Ljava/lang/String;ZLjava/lang/ClassLoader;)Ljava/lang/Class;",
+	   forName0, 3, false);
 }
