5d10c46b33421387cc8dbf731737e1ed2e403491
kornstae <none@none>
2002-09-24 09:51:42 +0100

    Moved dump methods to a class Dump in the default package.

---
+++ vm-stockwerk/java/Dump.java
@@ -0,0 +1,17 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+public class Dump {
+    public static native void dump(int i);
+    public static native void dump(Object s);
+    public static native void dump(String s);
+}
