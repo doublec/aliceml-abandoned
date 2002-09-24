39c0c382bbf7ba825714f6741b9b8ae6d50904be
kornstae <none@none>
2002-09-24 09:50:40 +0100

    Implemented doPrivileged to always succeed without security checks.

--- vm-stockwerk/java/java/security/AccessController.cc
+++ vm-stockwerk/java/java/security/AccessController.cc
@@ -14,15 +14,21 @@
 #include "java/ClassLoader.hh"
 #include "java/Authoring.hh"
 
-//static word wPrivilegedActionRunMethodRef;
+static word wPrivilegedActionRunMethodRef;
 
 DEFINE1(doPrivileged) {
-  //--**
-  RETURN(null);
+  DECLARE_OBJECT(object, x0);
+  Assert(object != INVALID_POINTER);
+  InterfaceMethodRef *methodRef = 
+    InterfaceMethodRef::FromWord(wPrivilegedActionRunMethodRef);
+  if (methodRef == INVALID_POINTER) REQUEST(wPrivilegedActionRunMethodRef);
+  Closure *closure = object->GetClass()->
+    GetInterfaceMethod(methodRef->GetClass(), methodRef->GetIndex());
+  Assert(closure != INVALID_POINTER);
+  return Scheduler::PushCall(closure->ToWord());
 } END
 
 void NativeMethodTable::java_security_AccessController(JavaString *className) {
-  /*
   ClassLoader *classLoader = ClassLoader::GetBootstrapClassLoader();
   JavaString *privilegedActionName =
     JavaString::New("java/security/PrivilegedAction");
@@ -31,7 +37,6 @@ void NativeMethodTable::java_security_AccessController(JavaString *className) {
     (wPrivilegedAction, JavaString::New("run"),
      JavaString::New("()Ljava/lang/Object;"));
   RootSet::Add(wPrivilegedActionRunMethodRef);
-  */
   Register(className, "doPrivileged",
 	   "(Ljava/security/PrivilegedAction;)Ljava/lang/Object;",
 	   doPrivileged, 1, false);
