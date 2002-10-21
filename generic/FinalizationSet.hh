db4e7ee2996e0eefad10cfb1087a64be63f59187
bruni <none@none>
2002-10-21 16:11:48 +0100

    Use templates for maps; adapt code to interface changes

--- generic/FinalizationSet.hh
+++ generic/FinalizationSet.hh
@@ -17,7 +17,7 @@
 #pragma interface "generic/FinalizationSet.hh"
 #endif
 
-#include "store/WeakDictionary.hh"
+#include "store/WeakMap.hh"
 #include "generic/RootSet.hh"
 
 class DllExport FinalizationSet: public Finalization {
@@ -29,21 +29,19 @@ private:
 public:
   FinalizationSet() {
     keyCounter = 0;
-    wWeakDictionary = WeakDictionary::New(initialSize, this)->ToWord();
+    wWeakDictionary = WeakMap::New(initialSize, this)->ToWord();
     RootSet::Add(wWeakDictionary);
   }
 
   u_int Register(word value) {
-    WeakDictionary *weakDictionary =
-      WeakDictionary::FromWordDirect(wWeakDictionary);
+    WeakMap *weakDictionary = WeakMap::FromWordDirect(wWeakDictionary);
     u_int key = keyCounter++;
-    weakDictionary->InsertItem(key, value);
+    weakDictionary->Put(Store::IntToWord(key), value);
     return key;
   }
   void Unregister(u_int key) {
-    WeakDictionary *weakDictionary =
-      WeakDictionary::FromWordDirect(wWeakDictionary);
-    weakDictionary->DeleteItem(key);
+    WeakMap *weakDictionary = WeakMap::FromWordDirect(wWeakDictionary);
+    weakDictionary->Remove(Store::IntToWord(key));
   }
   virtual void Finalize(word value) = 0;
 };
