bc63b2b6bb51bf3c1e858004361367bea4846937
bruni <none@none>
2002-10-21 16:07:17 +0100

    Use templates for maps

---
+++ vm-stockwerk/store/WeakMap.cc
@@ -0,0 +1,18 @@
+//
+// Author:
+//   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
+//
+// Copyright:
+//   Thorsten Brunklaus, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#if defined(INTERFACE)
+#pragma implementation "store/WeakMap.hh"
+#endif
+
+#include "store/WeakMap.hh"
+#include "store/MapNode.hh"
