3019a8a5cf13f12e7d2fe6083438a51ad70ff1c0
kornstae <none@none>
2002-10-27 21:28:21 +0000

    Replaced ALICE_EXTRA_LOAD by ALICE_LOAD_PREFIX/ALICE_LOAD_SUFFIX    (missing in .bat file).

--- vm-mozart/alicerun.bat
+++ vm-mozart/alicerun.bat
@@ -4,6 +4,8 @@ set OZ_LOAD=pattern=?{x}=?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;pat
 
 set ALICE_LOAD=pattern=x-oz:?{x}=x-oz:?{x};pattern=?{x}=?{x}.ozf;pattern=?{x}=?{x};pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}
 
+rem add ALICE_LOAD_PREFIX/ALICE_LOAD_SUFFIX #--**
+
 set OZ_LOAD=%ALICE_LOAD%;%OZ_LOAD%
 
 ozengine x-alice:/VMMain %1 %2 %3 %4 %5 %6 %7 %8 %9
