4da3de560b8b2322f26e87fe360b09eb0b9d69e0
grabow <none@none>
2002-12-04 15:02:51 +0000

    * added native function to get selected string in tree * GtkBuilder now correctly attaches event handler to tree

--- lib/gtk/seam/NativeGtkSpecial.hh
+++ lib/gtk/seam/NativeGtkSpecial.hh
@@ -47,14 +47,52 @@ DEFINE0(NativeGtk_gtkFalse) {
   RETURN(Store::IntToWord(FALSE));
 } END
 
+DEFINE0(NativeGtk_textIterNew) {
+  GtkTextIter *iter = new GtkTextIter;
+  RETURN(Store::UnmanagedPointerToWord(iter));
+} END
+
 DEFINE0(NativeGtk_treeIterNew) {
   GtkTreeIter *iter = new GtkTreeIter;
   RETURN(Store::UnmanagedPointerToWord(iter));
 } END
 
-DEFINE0(NativeGtk_textIterNew) {
-  GtkTextIter *iter = new GtkTextIter;
-  RETURN(Store::UnmanagedPointerToWord(iter));
+DEFINE0(NativeGtk_treeStoreNew) {
+  GtkTreeStore *store = gtk_tree_store_new(1, G_TYPE_STRING);
+  RETURN(Store::UnmanagedPointerToWord(store));
+} END
+
+DEFINE3(NativeGtk_treeModelGetStringAt) {
+  DECLARE_UNMANAGED_POINTER(mod, x0);
+  DECLARE_UNMANAGED_POINTER(path, x1);
+  DECLARE_INT(col, x2);
+  GtkTreeModel *model = static_cast<GtkTreeModel*>(mod);
+  gchar *result = "";
+  GValue *val;
+  GtkTreeIter *iter;
+  if (gtk_tree_model_get_iter(model, iter, static_cast<GtkTreePath*>(path))) {
+    gtk_tree_model_get_value(model, iter, col, val);
+    if (G_VALUE_TYPE(val) == G_TYPE_STRING)
+      result = g_value_dup_string(val);
+  }
+  RETURN(String::New(reinterpret_cast<const char *>(result))->ToWord());
+} END
+
+DEFINE1(NativeGtk_treeViewGetSelectedString) {
+  DECLARE_UNMANAGED_POINTER(t, x0);
+  GtkTreeView *tree = static_cast<GtkTreeView*>(t);
+  GtkTreeIter *iter;
+  GValue val;
+  memset(&val, 0, sizeof(GValue));
+  char *result = "";
+  if (gtk_tree_selection_get_selected(gtk_tree_view_get_selection(tree), 
+				      NULL, iter)) {
+    gtk_tree_model_get_value(gtk_tree_view_get_model(tree), iter, 0, &val);
+    if (G_VALUE_TYPE(&val) == G_TYPE_STRING)
+      result = g_value_dup_string(&val);
+    g_value_unset(&val);
+  }
+  RETURN(String::New(reinterpret_cast<const char*>(result))->ToWord());
 } END
 
 ////////////////////////////////////////////////////////////////////////
