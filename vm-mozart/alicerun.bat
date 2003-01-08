1ca68b1fc90e59f3efb46eebc8081349340e2b88
kornstae <none@none>
2003-01-08 23:20:35 +0000

    Simplified ALICE_LOAD environment variable, thanks to Url.toLocalFile.

--- vm-mozart/alicerun.bat
+++ vm-mozart/alicerun.bat
@@ -5,7 +5,7 @@ if not defined STOCKHOME set STOCKHOME=%PROGRAMFILES%\Alice
 
 set OZ_LOAD=pattern=?{x}=?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x};cache=%OZHOME%/cache
 
-set ALICE_LOAD=pattern=x-oz:?{x}=x-oz:?{x};pattern=?{x}=?{x}.ozf;pattern=?{x}=?{x};pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}
+set ALICE_LOAD=pattern=x-oz:?{x}=x-oz:?{x};pattern=?{x}=?{x}.ozf;pattern=?{x}=?{x}
 
 rem add ALICE_LOAD_PREFIX/ALICE_LOAD_SUFFIX #--**
 
