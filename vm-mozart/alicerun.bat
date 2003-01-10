badefb92b97faec1693ad6422cad87741f264c25
kornstae <none@none>
2003-01-10 13:56:12 +0000

    Adapted setting of OZ_LOAD and ALICE_LOAD to what the /bin/sh version does; In particular, added ALICE_LOAD_PREFIX and ALICE_LOAD_SUFFIX handling.

--- vm-mozart/alicerun.bat
+++ vm-mozart/alicerun.bat
@@ -3,12 +3,13 @@
 if not defined OZHOME set OZHOME=%PROGRAMFILES%\Mozart
 if not defined STOCKHOME set STOCKHOME=%PROGRAMFILES%\Alice
 
-set OZ_LOAD=pattern=?{x}=?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;pattern=x-alice:/?{x}=%STOCKHOME%/?{x};cache=%OZHOME%/cache
-
 set ALICE_LOAD=pattern=x-oz:?{x}=x-oz:?{x};pattern=?{x}=?{x}.ozf;pattern=?{x}=?{x}
 
-rem add ALICE_LOAD_PREFIX/ALICE_LOAD_SUFFIX #--**
+if defined ALICE_LOAD_PREFIX set ALICE_LOAD=%ALICE_LOAD_PREFIX%;%ALICE_LOAD%
+if defined ALICE_LOAD_SUFFIX set ALICE_LOAD=%ALICE_LOAD%;%ALICE_LOAD_PREFIX%
 
-set OZ_LOAD=%ALICE_LOAD%;%OZ_LOAD%
+if not defined OZ_LOAD set OZ_LOAD=cache=%HOMEDRIVE%%HOMEPATH%/.oz;cache=%OZHOME%/cache
+
+set OZ_LOAD=%ALICE_LOAD%;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;%OZ_LOAD%
 
 ozengine x-alice:/VMMain %1 %2 %3 %4 %5 %6 %7 %8 %9
