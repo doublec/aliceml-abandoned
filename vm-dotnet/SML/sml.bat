0b5b80ab3290af84088113df82a02cda3e420422
kornstae <none@none>
2003-01-13 16:34:28 +0000

    Renamed STOCKHOME to ALICE_HOME.

--- vm-dotnet/SML/sml.bat
+++ vm-dotnet/SML/sml.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"%STOCKHOME%\SML\run.x86-win32.exe" @SMLload="%STOCKHOME%\SML\sml.x86-win32" %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%ALICE_HOME%\SML\run.x86-win32.exe" @SMLload="%ALICE_HOME%\SML\sml.x86-win32" %1 %2 %3 %4 %5 %6 %7 %8 %9
