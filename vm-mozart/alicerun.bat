c862de941ce3d8fb823fbbf8ccf8fc899fd9107a
kornstae <none@none>
2003-01-13 16:35:18 +0000

    Renamed STOCKHOME to ALICE_HOME.

--- vm-mozart/alicerun.bat
+++ vm-mozart/alicerun.bat
@@ -1,7 +1,7 @@
 @echo off
 
 if not defined OZHOME set OZHOME=%PROGRAMFILES%\Mozart
-if not defined STOCKHOME set STOCKHOME=%PROGRAMFILES%\Alice
+if not defined ALICE_HOME set ALICE_HOME=%PROGRAMFILES%\Alice
 
 set ALICE_LOAD=cache=%HOMEDRIVE%%HOMEPATH%/.alice/cache;pattern=x-oz:?{x}=x-oz:?{x};pattern=?{x}=?{x}.ozf;pattern=?{x}=?{x}
 
@@ -10,6 +10,6 @@ if defined ALICE_LOAD_SUFFIX set ALICE_LOAD=%ALICE_LOAD%;%ALICE_LOAD_PREFIX%
 
 if not defined OZ_LOAD set OZ_LOAD=cache=%HOMEDRIVE%%HOMEPATH%/.oz/cache;cache=%OZHOME%/cache
 
-set OZ_LOAD=%ALICE_LOAD%;pattern=x-alice:/?{x}=%STOCKHOME%/?{x}.ozf;%OZ_LOAD%
+set OZ_LOAD=%ALICE_LOAD%;pattern=x-alice:/?{x}=%ALICE_HOME%/?{x}.ozf;%OZ_LOAD%
 
 ozengine x-alice:/VMMain %1 %2 %3 %4 %5 %6 %7 %8 %9
