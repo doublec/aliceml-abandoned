b61f3de92a89e1db87d9eb752e87993e52712b77
grabow <none@none>
2003-01-23 19:23:20 +0000

    * GdkEvents now produce the correct EVENT datatype on the stream * include file mechanism improved * more freedom to specify ignore list

--- lib/gtk/seam/NativeGtkSpecial.hh
+++ lib/gtk/seam/NativeGtkSpecial.hh
@@ -1,5 +1,9 @@
 #ifndef _NATIVE_GTK_SPECIAL_HH_
-#define _NATIVE_GTK_SPECIAL_HH_ { 2, NativeGtkSpecialInit }
+#define _NATIVE_GTK_SPECIAL_HH_
+
+#include "Alice.hh"
+#include "MyNativeAuthoring.hh"
+#include <gtk/gtk.h>
 
 DEFINE1(NativeGtk_TypeMismatch) {
   Constructor *ccVal = Constructor::FromWord(TypeMismatchConstructor);
@@ -15,24 +19,8 @@ void NativeGtkSpecialInit(Record *record) {
 }
 
 DEFINE0(NativeGtk_init) {
-  /*
-  DECLARE_GLIST(argvlist, Properties::commandLineArguments, GList, g_list, 
-		DECLARE_CSTRING);
-  int argc = g_list_length(argvlist);
-  printf("**length: %d", argc);
-  char **argv = new (char*)[argc];
-  for (int i = 0; i < argc; i++) {
-    printf("**i: %d", i);
-    argv[i] = reinterpret_cast<char*>(g_list_nth_data(argvlist, i));
-  }
-  gtk_init(&argc,&argv);
-  delete [] argv;
-  */
-
   gtk_init(NULL,NULL);
-
   RETURN_UNIT;
-
 } END
 
 DEFINE0(NativeGtk_null) {
@@ -127,10 +115,270 @@ word create_param(int tag, word value) {
   return param->ToWord();
 }
 
-word GdkEventToEventDatatype(GdkEvent *event) {
-  TagVal *ev = TagVal::New(0,1);
-  ev->Init(0,Store::UnmanagedPointerToWord(gdk_event_copy(event))); 
-  return ev->ToWord();
+word GdkScrollDirectionToDatatype(GdkScrollDirection dir) {
+  enum { SCROLL_DOWN, SCROLL_LEFT, SCROLL_RIGHT, SCROLL_UP };
+  switch (dir) {
+  case GDK_SCROLL_UP: return Store::IntToWord(SCROLL_UP);
+  case GDK_SCROLL_DOWN: return Store::IntToWord(SCROLL_DOWN);
+  case GDK_SCROLL_LEFT: return Store::IntToWord(SCROLL_LEFT);
+  }
+  return Store::IntToWord(SCROLL_RIGHT);
+}
+
+word GdkCrossingModeToDatatype(GdkCrossingMode mode) {
+  enum { CROSSING_GRAB, CROSSING_NORMAL, CROSSING_UNGRAB };
+  switch (mode) {
+  case GDK_CROSSING_NORMAL: return Store::IntToWord(CROSSING_NORMAL);
+  case GDK_CROSSING_GRAB: return Store::IntToWord(CROSSING_GRAB);
+  }
+  return Store::IntToWord(CROSSING_UNGRAB);
+}
+
+word GdkNotifyTypeToDatatype(GdkNotifyType type) {
+  enum { NOTIFY_ANCESTOR, NOTIFY_INFERIOR, NOTIFY_NONLINEAR, 
+	 NOTIFY_NONLINEAR_VIRTUAL, NOTIFY_UNKNOWN, NOTIFY_VIRTUAL };
+  switch (type) {
+  case GDK_NOTIFY_ANCESTOR: return Store::IntToWord(NOTIFY_ANCESTOR);
+  case GDK_NOTIFY_VIRTUAL: return Store::IntToWord(NOTIFY_VIRTUAL);
+  case GDK_NOTIFY_INFERIOR: return Store::IntToWord(NOTIFY_INFERIOR);
+  case GDK_NOTIFY_NONLINEAR: return Store::IntToWord(NOTIFY_NONLINEAR);
+  case GDK_NOTIFY_NONLINEAR_VIRTUAL: 
+    return Store::IntToWord(NOTIFY_NONLINEAR_VIRTUAL);
+  }
+  return Store::IntToWord(NOTIFY_UNKNOWN);
+}
+
+word GdkVisibilityStateToDatatype(GdkVisibilityState state) {
+  enum {  VISIBILITY_FULLY_OBSCURED, VISIBILITY_PARTIAL, 
+	  VISIBILITY_UNOBSCURED };
+  switch (state) {
+  case GDK_VISIBILITY_FULLY_OBSCURED: 
+    return Store::IntToWord(VISIBILITY_FULLY_OBSCURED);
+  case GDK_VISIBILITY_PARTIAL: return Store::IntToWord(VISIBILITY_PARTIAL);
+  }
+  return Store::IntToWord(VISIBILITY_UNOBSCURED);
+}
+
+word GdkEventToDatatype(GdkEvent *event) {
+  enum { _2BUTTON_PRESS, _3BUTTON_PRESS, BUTTON_PRESS, BUTTON_RELEASE, 
+	 CLIENT_EVENT,
+	 CONFIGURE,
+	 DELETE, DESTROY,
+	 DRAG_ENTER, DRAG_LEAVE, DRAG_MOTION, DRAG_STATUS,
+	 DROP_FINISHED, DROP_START, 
+	 ENTER_NOTIFY, 
+	 EXPOSE,
+	 FOCUS_CHANGE, 
+	 KEY_PRESS, KEY_RELEASE,
+	 LEAVE_NOTIFY, 
+	 MAP,
+	 MOTION_NOTIFY,
+	 NO_EXPOSE,
+	 NOTHING,
+	 PROPERTY_NOTIFY,
+	 PROXIMITY_IN, PROXIMITY_OUT,
+	 SCROLL, 
+	 SELECTION_CLEAR, SELECTION_NOTIFY, SELECTION_REQUEST, 
+	 SETTING, 
+	 UNMAP,
+	 UNSUPPORTED, 
+	 VISIBILITY_NOTIFY, 
+	 WINDOW_STATE };
+  int evtype = UNSUPPORTED;
+  Record *r = NULL;
+
+  switch (event->type) {
+  case GDK_NOTHING: evtype = NOTHING; break;
+  case GDK_DELETE: evtype = DELETE; break;
+  case GDK_DESTROY: evtype = DESTROY; break;
+  case GDK_EXPOSE: evtype = EXPOSE; break;
+  case GDK_MOTION_NOTIFY: evtype = MOTION_NOTIFY; break;
+  case GDK_BUTTON_PRESS:   evtype = BUTTON_PRESS; break;
+  case GDK_2BUTTON_PRESS:  evtype = _2BUTTON_PRESS; break;
+  case GDK_3BUTTON_PRESS:  evtype = _3BUTTON_PRESS; break;
+  case GDK_BUTTON_RELEASE: evtype = BUTTON_RELEASE; break;
+  case GDK_KEY_PRESS: evtype = KEY_PRESS; break;
+  case GDK_KEY_RELEASE: evtype = KEY_RELEASE; break;
+  case GDK_ENTER_NOTIFY: evtype = ENTER_NOTIFY; break;
+  case GDK_LEAVE_NOTIFY: evtype = LEAVE_NOTIFY; break;
+  case GDK_FOCUS_CHANGE: evtype = FOCUS_CHANGE; break;
+  case GDK_CONFIGURE: evtype = CONFIGURE; break;
+  case GDK_MAP: evtype = MAP; break;
+  case GDK_UNMAP: evtype = UNMAP; break;
+  case GDK_PROPERTY_NOTIFY: evtype = PROPERTY_NOTIFY; break;
+  case GDK_SELECTION_CLEAR: evtype = SELECTION_CLEAR; break;
+  case GDK_SELECTION_REQUEST: evtype = SELECTION_REQUEST; break;
+  case GDK_SELECTION_NOTIFY: evtype = SELECTION_NOTIFY; break;
+  case GDK_PROXIMITY_IN: evtype = PROXIMITY_IN; break;
+  case GDK_PROXIMITY_OUT: evtype = PROXIMITY_OUT; break;
+  case GDK_DRAG_ENTER: evtype = DRAG_ENTER; break;
+  case GDK_DRAG_LEAVE: evtype = DRAG_LEAVE; break;
+  case GDK_DRAG_MOTION: evtype = DRAG_MOTION; break;
+  case GDK_DRAG_STATUS: evtype = DRAG_STATUS; break;
+  case GDK_DROP_START: evtype = DROP_START; break;
+  case GDK_DROP_FINISHED: evtype = DROP_FINISHED; break;
+  case GDK_CLIENT_EVENT: evtype = CLIENT_EVENT; break;
+  case GDK_VISIBILITY_NOTIFY: evtype = VISIBILITY_NOTIFY; break;
+  case GDK_NO_EXPOSE: evtype = NO_EXPOSE; break;
+  case GDK_SCROLL: evtype = SCROLL; break;
+  case GDK_WINDOW_STATE: evtype = WINDOW_STATE; break;
+  case GDK_SETTING: evtype = SETTING; break;
+}
+
+  g_print("event: Gdk: %d, Alice: %d\n", event->type, evtype);
+
+  switch (event->type) {    
+
+  case GDK_EXPOSE: {
+    GdkEventExpose *ev = reinterpret_cast<GdkEventExpose*>(event);
+    r = Record::New(8);
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("area_x", Store::IntToWord((ev->area).x));
+    r->Init("area_y", Store::IntToWord((ev->area).y));
+    r->Init("area_width", Store::IntToWord((ev->area).width));
+    r->Init("area_height", Store::IntToWord((ev->area).height));
+    r->Init("region", Store::UnmanagedPointerToWord(ev->region));
+    r->Init("count", Store::IntToWord(ev->count));
+    break;
+  }
+
+  case GDK_MOTION_NOTIFY: {
+    GdkEventMotion *ev = reinterpret_cast<GdkEventMotion*>(event);
+    r = Record::New(10);
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("time", Store::IntToWord(ev->time));
+    r->Init("x", Real::New(ev->x)->ToWord());
+    r->Init("y", Real::New(ev->y)->ToWord());
+    r->Init("state", Store::IntToWord(ev->state));
+    r->Init("is_hint", Store::IntToWord(ev->is_hint));
+    r->Init("device", Store::UnmanagedPointerToWord(ev->device));
+    r->Init("x_root", Real::New(ev->x_root)->ToWord());
+    r->Init("y_root", Real::New(ev->y_root)->ToWord());
+    break;
+  }
+
+  case GDK_BUTTON_PRESS:
+  case GDK_2BUTTON_PRESS:
+  case GDK_3BUTTON_PRESS:
+  case GDK_BUTTON_RELEASE: {
+    GdkEventButton *ev = reinterpret_cast<GdkEventButton*>(event);
+    r = Record::New(10);    
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("time", Store::IntToWord(ev->time));
+    r->Init("x", Real::New(ev->x)->ToWord());
+    r->Init("y", Real::New(ev->y)->ToWord());
+    r->Init("state", Store::IntToWord(ev->state));
+    r->Init("button", Store::IntToWord(ev->button));
+    r->Init("device", Store::UnmanagedPointerToWord(ev->device));
+    r->Init("x_root", Real::New(ev->x_root)->ToWord());
+    r->Init("y_root", Real::New(ev->y_root)->ToWord());
+    break;
+  }
+ 
+  case GDK_KEY_PRESS:
+  case GDK_KEY_RELEASE: {
+    GdkEventKey *ev = reinterpret_cast<GdkEventKey*>(event);
+    r = Record::New(9);
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("time", Store::IntToWord(ev->time));
+    r->Init("state", Store::IntToWord(ev->state));
+    r->Init("keyval", Store::IntToWord(ev->keyval));
+    r->Init("length", Store::IntToWord(ev->length));
+    r->Init("string", String::New(ev->string)->ToWord());
+    r->Init("hardware_keycode", Store::IntToWord(ev->hardware_keycode));
+    r->Init("group", Store::IntToWord(ev->group));
+    break;
+  }
+
+  case GDK_ENTER_NOTIFY: 
+  case GDK_LEAVE_NOTIFY: {
+    GdkEventCrossing *ev = reinterpret_cast<GdkEventCrossing*>(event);    
+    r = Record::New(12);
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("subwindow", Store::UnmanagedPointerToWord(ev->subwindow));
+    r->Init("time", Store::IntToWord(ev->time));
+    r->Init("x", Real::New(ev->x)->ToWord());
+    r->Init("y", Real::New(ev->y)->ToWord());
+    r->Init("x_root", Real::New(ev->x_root)->ToWord());
+    r->Init("y_root", Real::New(ev->y_root)->ToWord());
+    r->Init("mode", GdkCrossingModeToDatatype(ev->mode));
+    r->Init("detail", GdkNotifyTypeToDatatype(ev->detail));
+    r->Init("focus", BOOL_TO_WORD(ev->focus));
+    r->Init("state", Store::IntToWord(ev->state));
+    break;
+  }
+
+  case GDK_FOCUS_CHANGE: {
+    GdkEventFocus *ev = reinterpret_cast<GdkEventFocus*>(event);
+    r = Record::New(3);
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("hasFocus", BOOL_TO_WORD(ev->in));
+    break;
+  }
+
+  case GDK_CONFIGURE: {
+    GdkEventConfigure *ev = reinterpret_cast<GdkEventConfigure*>(event);
+    r = Record::New(6);    
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("x", Store::IntToWord(ev->x));
+    r->Init("y", Store::IntToWord(ev->y));
+    r->Init("width", Store::IntToWord(ev->width));
+    r->Init("height", Store::IntToWord(ev->height));
+    break;
+  }
+    
+  case GDK_VISIBILITY_NOTIFY: {
+    GdkEventVisibility *ev = reinterpret_cast<GdkEventVisibility*>(event);
+    r = Record::New(3);
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("state", GdkVisibilityStateToDatatype(ev->state));
+    break;
+  }
+
+  case GDK_NO_EXPOSE: {
+    GdkEventNoExpose *ev = reinterpret_cast<GdkEventNoExpose*>(event);
+    r = Record::New(2);
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    break;
+  }
+
+  case GDK_SCROLL: {
+    GdkEventScroll *ev = reinterpret_cast<GdkEventScroll*>(event);    
+    r = Record::New(10);
+    r->Init("window", Store::UnmanagedPointerToWord(ev->window));
+    r->Init("send", BOOL_TO_WORD(ev->send_event));
+    r->Init("time", Store::IntToWord(ev->time));
+    r->Init("x", Real::New(ev->x)->ToWord());
+    r->Init("y", Real::New(ev->y)->ToWord());
+    r->Init("state", Store::IntToWord(ev->state));
+    r->Init("direction", GdkScrollDirectionToDatatype(ev->direction));
+    r->Init("device", Store::UnmanagedPointerToWord(ev->device));
+    r->Init("x_root", Real::New(ev->x_root)->ToWord());
+    r->Init("y_root", Real::New(ev->y_root)->ToWord());
+    break;
+  }
+  }
+
+  /*  TagVal *tv = TagVal::New(evtype, (r ? 1 : 0));
+  if (r)
+    tv->Init(0, r->ToWord());
+  */
+
+  // *** workaround
+  evtype = UNSUPPORTED;
+  TagVal *tv = TagVal::New(evtype, 1);
+  tv->Init(0, Store::UnmanagedPointerToWord(event));
+
+  return tv->ToWord();
 }
 
 word create_object(GType t, gpointer p) {
@@ -152,7 +400,7 @@ word create_object(GType t, gpointer p) {
     else
       if (t == GDK_EVENT_TYPE) {
 	tag = EVENT;
-	value = GdkEventToEventDatatype(static_cast<GdkEvent*>(p));
+	value = GdkEventToDatatype(static_cast<GdkEvent*>(p));
       }
       else
 	value = Store::UnmanagedPointerToWord(p);
