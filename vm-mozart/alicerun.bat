499f4f136ae79131be1435dc235e6b44a30aab54
bruni <none@none>
2003-02-17 10:45:34 +0000

    Removed win2000 syntax.

--- vm-mozart/alicerun.bat
+++ vm-mozart/alicerun.bat
@@ -1,14 +1,11 @@
 @echo off
 
-if not defined OZHOME set OZHOME=%PROGRAMFILES%\Mozart
-if not defined ALICE_HOME set ALICE_HOME=%PROGRAMFILES%\Alice
-
 set ALICE_LOAD=cache=%HOMEDRIVE%%HOMEPATH%/.alice/cache;pattern=x-oz:?{x}=x-oz:?{x};pattern=?{x}=?{x}.ozf;pattern=?{x}=?{x}
 
-if defined ALICE_LOAD_PREFIX set ALICE_LOAD=%ALICE_LOAD_PREFIX%;%ALICE_LOAD%
-if defined ALICE_LOAD_SUFFIX set ALICE_LOAD=%ALICE_LOAD%;%ALICE_LOAD_PREFIX%
+if not "%ALICE_LOAD_PREFIX%"=="" set ALICE_LOAD=%ALICE_LOAD_PREFIX%;%ALICE_LOAD%
+if not "%ALICE_LOAD_SUFFIX%"=="" set ALICE_LOAD=%ALICE_LOAD%;%ALICE_LOAD_PREFIX%
 
-if not defined OZ_LOAD set OZ_LOAD=cache=%HOMEDRIVE%%HOMEPATH%/.oz/cache;cache=%OZHOME%/cache
+if "%OZ_LOAD%"=="" set OZ_LOAD=cache=%HOMEDRIVE%%HOMEPATH%/.oz/cache;cache=%OZHOME%/cache
 
 set OZ_LOAD=%ALICE_LOAD%;pattern=x-alice:/?{x}=%ALICE_HOME%/?{x}.ozf;%OZ_LOAD%
 
