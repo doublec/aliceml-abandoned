0d25b8581654091535ee3797c76f6e4e7616eded
kornstae <none@none>
2003-06-18 22:53:03 +0100

    Updated.

--- clean-seam.sh
+++ clean-seam.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-rm -f bootstrap/alicec-seam.$PLATFORM bootstrap/alicedep.$PLATFORM
+case `uname -s` in
+    CYGWIN*)
+	WINDOWS=1
+	SMLPLATFORM=x86-win32
+	;;
+    *)
+	WINDOWS=0
+	SMLPLATFORM=x86-linux
+	;;
+esac
+
+rm -f bootstrap/alicec-seam.$SMLPLATFORM bootstrap/alicedep.$SMLPLATFORM
 (cd bootstrap && make clean) || exit 1
-(cd vm-stockwerk && make clean) || exit
-(cd vm-stockwerk && make -f Makefile.bootstrap distclean) || exit 1
+(cd vm-seam && make clean WINDOWS=$WINDOWS) || exit
+(cd vm-seam && make -f Makefile.bootstrap distclean) || exit 1
 (cd lib/distribution && make TARGET=seam distclean) || exit 1
