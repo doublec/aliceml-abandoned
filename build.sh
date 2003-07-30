741f71a763fa0e04febeef8ec997fad5f8ef5c1a
kuhlmann <none@none>
2003-07-30 15:50:04 +0100

    Added build procedure for Automake/SEAM.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+
+AUTOMAKE="yes"
 LIGHTNING=1
 SUPPORTDIR="$(pwd)"
 : ${prefix="$SUPPORTDIR/install"}
@@ -12,6 +15,37 @@ case `uname -s` in
 	CC=gcc
 	;;
 esac
+
+##
+## Build Support Libraries: Automake/SEAM
+##
+
+## Automake version to use
+amversion="1.7.6"
+
+if [ "x${AUTOMAKE}" != "x" ]; then
+    if test -d "${SUPPORTDIR}/automake-seam/automake-${amversion}"; then
+	echo "### Automake/SEAM has been configured already." >&2
+	echo "### Please delete the following directory to re-configure it:" >&2
+	echo "### ${SUPPORTDIR}/automake-seam/automake-${amversion}" >&2
+    else
+	echo "### Building Automake/SEAM" >&2
+	cd "${SUPPORTDIR}/automake-seam" && (
+	    echo "### - extracting the source" >&2
+	    tar xzf automake-${amversion}.tar.gz
+	    cd automake-${amversion} && (
+		echo "### - applying SEAM-specific patches" >&2
+		patch -p1 < ../automake-${amversion}.seam.patch
+		patch -p1 < ../automake2automake-seam.patch
+		echo "### - reconfiguring the source" >&2
+		aclocal; automake; autoconf
+		./configure --prefix="${prefix}" &&
+		echo "### - building and installing" >&2
+		make all install
+	    )
+	)
+    fi
+fi
 
 ##
 ## Build Support Libraries: Lightning
