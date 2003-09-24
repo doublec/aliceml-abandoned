e018e6affbd9fd16f2cd2d0c3af0d6647a79b0a6
woop <none@none>
2003-09-24 13:10:38 +0100

    *** empty log message ***

--- ffigen/sample/util.h
+++ ffigen/sample/util.h
@@ -1,6 +1,6 @@
 
-//#include <stdio.h>
-//#include <gtk/gtk.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
 
 void printhello();
 
@@ -31,9 +31,9 @@ myenum dummy(myenum p);
 void mymemcpy(void* dst, const void* src, int length);
 void mymemset(void* dst, int length, char chr);
 
-void inc(int* i);
+void inc(int* i,...);
 
-//int list_add(GList* lst);
+int list_add(GList* lst);
 
 typedef void (*voidtovoid)(void);
 voidtovoid get_printhello();
