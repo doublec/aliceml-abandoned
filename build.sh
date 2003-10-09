f5e1579faf447b8ed81c95f1b9a848210aa35b8f
tack <none@none>
2003-10-09 13:54:50 +0100

    Added lightning 1.1 with appropriate patch.

--- build.sh
+++ build.sh
@@ -54,16 +54,32 @@ fi
 ##
 ## Build Support Libraries: Lightning
 ##
+
+# lightning version to use
+lightningversion=1.1
+
 if [ "$LIGHTNING" -ne 0 ]
 then
-    if [ ! -f "$prefix/include/lightning.h" ]
-    then
-	mkdir -p "$SUPPORTDIR/build/lightning" 2>/dev/null
-	(
-	    cd "$SUPPORTDIR/build/lightning" &&
-	    "$SUPPORTDIR/lightning/configure" CC="$CC" --prefix="$prefix" &&
-	    make all install
-	) || exit 1
+    if test -d "${SUPPORTDIR}/lightning/lightning-${lightningversion}"; then
+	echo "### Lightning/SEAM has been configured already." >&2
+	echo "### Please delete the following directory to re-configure it:" >&2
+	echo "### ${SUPPORTDIR}/lightning/lightning-${lightningversion}" >&2
+    else
+	echo "### Building lightning/SEAM" >&2
+	cd "${SUPPORTDIR}/lightning" && (
+	    echo "### - extracting the source" >&2
+	    tar xzf lightning-${lightningversion}.tar.gz
+	    cd lightning-${lightningversion} && (
+		echo "### - applying SEAM-specific patches" >&2
+		patch -p1 < ../lightning-${lightningversion}.seam.patch
+		echo "### - reconfiguring the source" >&2
+		mkdir -p "${SUPPORTDIR}/build/lightning" 2>/dev/null
+		cd "${SUPPORTDIR}/build/lightning" &&
+		"${SUPPORTDIR}/lightning/lightning-${lightningversion}/configure" CC="$CC" --prefix="$prefix" &&
+		echo "### - building and installing" >&2 &&
+		make all install
+	    )
+	)
     fi
 fi
 
