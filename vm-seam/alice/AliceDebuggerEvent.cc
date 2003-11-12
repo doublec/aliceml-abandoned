946378918b3a67526e704f811bc35f7764f7face
jens <none@none>
2003-11-12 13:55:52 +0000

    Merged from Trunk

---
+++ vm-seam/alice/AliceDebuggerEvent.cc
@@ -0,0 +1,23 @@
+//
+// Author:
+//   Jens Regenberg <jens@ps.uni-sb.de>
+//
+// Copyright:
+//   Jens Regenberg, 2000-2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#if DEBUGGER
+#if defined(INTERFACE)
+#pragma implementation "alice/AliceDebuggerEvent.hh"
+#endif
+
+#include "alice/AliceDebuggerEvent.hh"
+
+AliceEventAccessor *AliceEventAccessor::self;
+
+AliceEventAccessor::AliceEventAccessor() {}
+#endif
