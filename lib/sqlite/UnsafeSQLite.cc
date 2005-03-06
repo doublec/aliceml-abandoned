ca67707451c9858e6b43894f960a290628a828d5
tack <none@none>
2005-03-06 15:34:24 +0000

    Updates to SQLite version 3.

--- lib/sqlite/UnsafeSQLite.cc
+++ lib/sqlite/UnsafeSQLite.cc
@@ -12,19 +12,19 @@
 
 #include "Alice.hh"
 
-#include <sqlite.h>
+#include <sqlite3.h>
 
 #define DECLARE_SQLITEDB(db, x)                       \
-  sqlite *db;                                                  \
+  sqlite3 *db;                                                  \
   if (Store::WordToTransient(x) != INVALID_POINTER) { REQUEST(x); } \
   { ConcreteRepresentation *cr = ConcreteRepresentation::FromWordDirect(x); \
-    db = (sqlite *) Store::WordToUnmanagedPointer(cr->Get(0)); }
+    db = (sqlite3 *) Store::WordToUnmanagedPointer(cr->Get(0)); }
 
-#define DECLARE_SQLITEVM(vm, x)                       \
-  sqlite_vm *vm;                                                  \
+#define DECLARE_SQLITESTMT(stmt, x)                       \
+  sqlite3_stmt *stmt;                                                  \
   if (Store::WordToTransient(x) != INVALID_POINTER) { REQUEST(x); } \
   { ConcreteRepresentation *cr = ConcreteRepresentation::FromWordDirect(x); \
-    vm = (sqlite_vm *) Store::WordToUnmanagedPointer(cr->Get(0)); }
+    stmt = (sqlite3_stmt *) Store::WordToUnmanagedPointer(cr->Get(0)); }
 
 
 class SQLiteHandler : public ConcreteRepresentationHandler {
@@ -59,12 +59,12 @@ static word MakeSQLError(String *err) {
 
 DEFINE1(sql_open) {
   DECLARE_STRING(filename, x0);
-  char *errmsg;
 
-  sqlite *db = sqlite_open(filename->ExportC(), 0, &errmsg);
-  if (db==NULL) {
+  sqlite3 *db;
+  int ret = sqlite3_open(filename->ExportC(), &db);
+  if (ret!=SQLITE_OK) {
+    const char *errmsg = sqlite3_errmsg(db);
     word exn = MakeSQLError(String::New(errmsg));
-    free(errmsg);
     RAISE(exn);
   }
 
@@ -77,67 +77,67 @@ DEFINE1(sql_open) {
 
 DEFINE1(sql_close) {
   DECLARE_SQLITEDB(db, x0);
-  sqlite_close(db);
+  int ret = sqlite3_close(db);
+  if (ret!=SQLITE_OK) {
+    const char *errmsg = sqlite3_errmsg(db);
+    word exn = MakeSQLError(String::New(errmsg));
+    RAISE(exn);
+  }
   RETURN_UNIT;
 } END
 
-DEFINE2(sql_compile) {
+DEFINE2(sql_prepare) {
   DECLARE_SQLITEDB(db, x0);
   DECLARE_STRING(sqlstatement, x1);
 
-  sqlite_vm *ppVm;
+  sqlite3_stmt *stmt;
   const char *pzTail;
-  char *errmsg;
-  int res = sqlite_compile(db,
-			   sqlstatement->ExportC(),
-			   &pzTail,
-			   &ppVm,
-			   &errmsg);
+  int res = sqlite3_prepare(db,
+                            sqlstatement->ExportC(),
+                            sqlstatement->GetSize(),
+                            &stmt,
+                            &pzTail);
   if (res!=SQLITE_OK) {
+    const char *errmsg = sqlite3_errmsg(db);
     word exn = MakeSQLError(String::New(errmsg));
-    free(errmsg);
     RAISE(exn);    
   }
 
   ConcreteRepresentation *cr = ConcreteRepresentation::New(sqliteHandler, 2);
-  cr->Init(0, Store::UnmanagedPointerToWord(ppVm));
+  cr->Init(0, Store::UnmanagedPointerToWord(stmt));
   cr->Init(1, Store::IntToWord(1));
   sqliteFinalizationSet->Register(cr->ToWord());
   RETURN(cr->ToWord());
 } END
 
 DEFINE1(sql_step) {
-  DECLARE_SQLITEVM(vm, x0);
-  int cols;
-  const char **pazValue;
-  const char **pazColName;
-  int retcode = sqlite_step(vm,
-			    &cols,
-			    &pazValue,
-			    &pazColName);
+  DECLARE_SQLITESTMT(stmt, x0);
+  int ret = sqlite3_step(stmt);
 
-  switch(retcode) {
+  switch(ret) {
   case SQLITE_BUSY:
     RETURN_INT(0); // datatype res = BUSY | ...
     break;
   case SQLITE_ROW:
     {
-      Vector *values = Vector::New(cols);
-      Vector *columnNames = Vector::New(cols);
-      for (u_int i=0; i<cols; i++) {
-	if (pazValue[i]==0) {
-	  values->Init(i, Store::IntToWord(0));
-	} else {
-	  TagVal *tv = TagVal::New(1,1);
-	  tv->Init(0, String::New(pazValue[i])->ToWord());
-	  values->Init(i, tv->ToWord());
-	}
-	columnNames->Init(i, String::New(pazColName[i])->ToWord());
-      }
-      TagVal *ret = TagVal::New(2, 2); // datatype res = ... | ROW of ...
-      ret->Init(0, values->ToWord());
-      ret->Init(1, columnNames->ToWord());
-      RETURN(ret->ToWord());
+      RETURN_INT(2);
+      break;
+//       Vector *values = Vector::New(cols);
+//       Vector *columnNames = Vector::New(cols);
+//       for (u_int i=0; i<cols; i++) {
+// 	if (pazValue[i]==0) {
+// 	  values->Init(i, Store::IntToWord(0));
+// 	} else {
+// 	  TagVal *tv = TagVal::New(1,1);
+// 	  tv->Init(0, String::New(pazValue[i])->ToWord());
+// 	  values->Init(i, tv->ToWord());
+// 	}
+// 	columnNames->Init(i, String::New(pazColName[i])->ToWord());
+//       }
+//       TagVal *ret = TagVal::New(2, 2); // datatype res = ... | ROW of ...
+//       ret->Init(0, values->ToWord());
+//       ret->Init(1, columnNames->ToWord());
+//       RETURN(ret->ToWord());
     }
     break;
   case SQLITE_DONE:
@@ -148,13 +148,25 @@ DEFINE1(sql_step) {
     RAISE(MakeSQLError(String::New("sqlite step error")));
     break;
   }
-
-
 } END
 
 DEFINE1(sql_finalize) {
-  DECLARE_SQLITEVM(vm, x0);
-  sqlite_finalize(vm, 0);
+  DECLARE_SQLITESTMT(stmt, x0);
+  int res = sqlite3_finalize(stmt);
+  if (res!=SQLITE_OK) {
+    word exn = MakeSQLError(String::New("sqlite finalizing error"));
+    RAISE(exn);    
+  }
+  RETURN_UNIT;
+} END
+
+DEFINE1(sql_reset) {
+  DECLARE_SQLITESTMT(stmt, x0);
+  int res = sqlite3_reset(stmt);
+  if (res!=SQLITE_OK) {
+    word exn = MakeSQLError(String::New("sqlite reset error"));
+    RAISE(exn);    
+  }
   RETURN_UNIT;
 } END
 
@@ -165,6 +177,72 @@ DEFINE3(sql_error) {
   RETURN(conVal->ToWord());
 } END
 
+/* Functions for accessing columns in a result row */
+
+DEFINE2(sql_c_blob) {
+  DECLARE_SQLITESTMT(stmt, x0);
+  DECLARE_INT(col, x1);
+
+  const char* blob = (const char*) sqlite3_column_blob(stmt, col);
+  RETURN(String::New(blob)->ToWord());
+} END
+
+DEFINE2(sql_c_int) {
+  DECLARE_SQLITESTMT(stmt, x0);
+  DECLARE_INT(col, x1);
+
+  int i = sqlite3_column_int(stmt, col);
+  RETURN_INT(i);
+} END
+
+DEFINE2(sql_c_real) {
+  DECLARE_SQLITESTMT(stmt, x0);
+  DECLARE_INT(col, x1);
+
+  double i = sqlite3_column_double(stmt, col);
+  RETURN(Real::New(i)->ToWord());
+} END
+
+DEFINE2(sql_c_text) {
+  DECLARE_SQLITESTMT(stmt, x0);
+  DECLARE_INT(col, x1);
+
+  const char* text = (char *)sqlite3_column_text(stmt, col);
+  RETURN(String::New(text)->ToWord());
+} END
+
+DEFINE2(sql_c_type) {
+  DECLARE_SQLITESTMT(stmt, x0);
+  DECLARE_INT(col, x1);
+
+  switch(sqlite3_column_type(stmt, col)) {
+  case SQLITE_INTEGER : RETURN_INT(1);
+  case SQLITE_FLOAT : RETURN_INT(3);
+  case SQLITE_TEXT : RETURN_INT(4);
+  case SQLITE_BLOB : RETURN_INT(0);
+  case SQLITE_NULL : RETURN_INT(2);
+  default:
+    word exn = MakeSQLError(String::New("unknown type"));
+    RAISE(exn);
+  }
+} END
+
+DEFINE2(sql_c_name) {
+  DECLARE_SQLITESTMT(stmt, x0);
+  DECLARE_INT(col, x1);
+
+  const char* text = sqlite3_column_name(stmt, col);
+  RETURN(String::New(text)->ToWord());
+} END
+
+DEFINE2(sql_c_count) {
+  DECLARE_SQLITESTMT(stmt, x0);
+  DECLARE_INT(col, x1);
+
+  int cols = sqlite3_column_count(stmt);
+  RETURN_INT(cols);
+} END
+
 word InitComponent() {
   sqliteFinalizationSet = new SQLiteFinalizationSet();
   sqliteHandler = new SQLiteHandler();
@@ -173,18 +251,35 @@ word InitComponent() {
     UniqueConstructor::New("SQLError", "UnsafeSQLite.SQLError")->ToWord();
   RootSet::Add(SQLErrorConstructor);
 
-  Record *record = Record::New(7);
+  Record *record = Record::New(15);
 
   INIT_STRUCTURE(record, "UnsafeSQLite", "opendb",
 		 sql_open, 1);
   INIT_STRUCTURE(record, "UnsafeSQLite", "closedb",
 		 sql_close, 1);
-  INIT_STRUCTURE(record, "UnsafeSQLite", "compile",
-		 sql_compile, 2);
+  INIT_STRUCTURE(record, "UnsafeSQLite", "prepare",
+		 sql_prepare, 2);
   INIT_STRUCTURE(record, "UnsafeSQLite", "step",
 		 sql_step, 1);
   INIT_STRUCTURE(record, "UnsafeSQLite", "finalize",
 		 sql_finalize, 1);
+  INIT_STRUCTURE(record, "UnsafeSQLite", "reset",
+		 sql_reset, 1);
+
+  INIT_STRUCTURE(record, "UnsafeSQLite", "c_blob",
+		 sql_c_blob, 2);
+  INIT_STRUCTURE(record, "UnsafeSQLite", "c_int",
+		 sql_c_int, 2);
+  INIT_STRUCTURE(record, "UnsafeSQLite", "c_real",
+		 sql_c_real, 2);
+  INIT_STRUCTURE(record, "UnsafeSQLite", "c_text",
+		 sql_c_text, 2);
+  INIT_STRUCTURE(record, "UnsafeSQLite", "c_type",
+		 sql_c_type, 2);
+  INIT_STRUCTURE(record, "UnsafeSQLite", "c_name",
+		 sql_c_name, 2);
+  INIT_STRUCTURE(record, "UnsafeSQLite", "c_count",
+		 sql_c_name, 1);
 
   record->Init("'SQLError", SQLErrorConstructor);
   INIT_STRUCTURE(record, "UnsafeSQLite", "SQLError",
