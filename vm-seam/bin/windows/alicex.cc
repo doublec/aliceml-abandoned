5eff9ec9a81d772dfa6454de03bb5d3aa97f8f28
rossberg <none@none>
2005-03-21 12:36:42 +0000

    * Attempt to work around Windows/Emacs/Gtk madnesss - only partially works... * Added alicerunwin, which does not run as a console application   (yeah, another incredible Windows stupidity...)

--- vm-seam/bin/windows/alicex.cc
+++ vm-seam/bin/windows/alicex.cc
@@ -1,9 +1,10 @@
 //
 // Authors:
 //   Thorsten Brunklaus <brunklaus@ps.uni-sb.de>
+//   Andreas Rossberg <rossberg@ps.uni-sb.de>
 //
 // Copyright:
-//   Thorsten Brunklaus, 2004
+//   Thorsten Brunklaus and Andreas Rossberg, 2004-2005
 //
 // Last Change:
 //   $Date$ by $Author$
@@ -28,6 +29,14 @@ static char *GetEnv(char *variable, char *defaults) {
 static void SetEnv(char *variable, char *value) {
   if (!SetEnvironmentVariable(variable, strdup(value))) {
     Error("Unable to set environment variable");
+  }
+}
+
+void trace(const char *s) {
+  static char *active = GetEnv("ALICE_TRACE_STARTUP", NULL);
+  if (active) {
+    fprintf(stderr, "[%s]\n", s);
+    fflush(stderr);
   }
 }
 
@@ -59,8 +68,53 @@ static char *usage() {
 }
 
 int main(int argc, char *argv[]) {
+  trace("InitSeam");
   InitSeam();
+  trace("InitAlice");
   InitAliceEnvironment();
+#if PRELOAD_GTK
+  // Workaround for the annoying problem we have with Windows+Emacs causing
+  // a deadlock on some machines when trying to load the Gtk DLL lazily.
+  // No idea why this happens (probably Gtk doing unrecommended stuff in DllMain,
+  // plus Emacs messing with almost every aspect of the environment); no idea
+  // why this fixes it either (partially...), but who wants to know anyway...
+  trace("LoadGtk");
+  static const char *dll[] = {
+    // "jpeg-62.dll", //
+    "libjpeg.dll", //
+    "freetype.dll",
+    "libgtk-win32-2.0-0.dll",
+    "iconv.dll",
+    "libintl-1.dll",
+    "libart_lgpl_2.dll",
+    // "libpangoft2-1.0-0.dll", // requires libfontconfig-1.dll
+    "libpango-1.0-0.dll",
+    "libatk-1.0-0.dll", 
+    "libgdk-win32-2.0-0.dll", 
+    "libpangowin32-1.0-0.dll",
+    "libgdk_pixbuf-2.0-0.dll",
+    "libpng.dll",
+    "libglib-2.0-0.dll",  
+    "libtiff.dll", //
+    "libgmodule-2.0-0.dll", 
+    "xmlparse.dll",
+    "libgnomecanvas-2.dll", 
+    "xmltok.dll",
+    "libgobject-2.0-0.dll",
+    "zlib.dll",
+    "libgthread-2.0-0.dll",
+    NULL
+    // Can we leave out some of these?
+  };
+  for (int i = 0; dll[i]; i++) {
+    trace(dll[i]);
+    if (!LoadLibrary(dll[i])) {
+      fprintf(stderr, "alice: couldn't load %s\n", dll[i]);
+      exit(1);
+    }
+  }
+#endif
+  trace("GetArgs");
   int brokerArgc = argc + 1;
   char *brokerArgv[brokerArgc];
   brokerArgv[0] = "alice";
@@ -68,6 +122,8 @@ int main(int argc, char *argv[]) {
   for (int i = 1; i < argc; i++)
     brokerArgv[i + 1] = argv[i];
   String *languageId = String::New(brokerArgv[0]);
+  trace("StartBroker");
   Broker::Start(languageId, brokerArgc, brokerArgv);
+  trace("RunScheduler");
   return Scheduler::Run();
 }
