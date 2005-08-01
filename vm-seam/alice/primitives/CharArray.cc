fd3a220796d604328ad19ed25f895f76481dab1d
tack <none@none>
2005-08-01 14:32:35 +0100

    Forgot to add

---
+++ vm-seam/alice/primitives/CharArray.cc
@@ -0,0 +1,17 @@
+//
+// Author:
+//   Leif Kornstaedt <kornstae@ps.uni-sb.de>
+//
+// Copyright:
+//   Leif Kornstaedt, 2002
+//
+// Last Change:
+//   $Date$ by $Author$
+//   $Revision$
+//
+
+#define XArray CharArray
+#define XVector CharVector
+#define REQUEST_ELEMS_INT 1
+
+#include "Array.icc"
