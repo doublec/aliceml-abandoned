f87a411c7b028c4e261a973a8d1d524cc452603f
bgrund <none@none>
2005-08-31 08:14:31 +0100

    Initial checkin of libglade binding.

---
+++ lib/gtk/seam/NativeGlade.cc
@@ -0,0 +1,67 @@
+#include <glade/glade.h>
+#include "MyNativeAuthoring.hh"
+#include "NativeUtils.hh"
+
+#define DECLARE_OPT_CSTRING(s, x) \
+    const char *s = 0;  \
+    if (Store::WordToTransient(x) != INVALID_POINTER) { REQUEST(x); }\
+    if (Store::WordToInt (x) == Types::NONE) { \
+        s = 0; \
+    } else { \
+        TagVal *tv = TagVal::FromWord (x); \
+        Assert(tv->GetTag () == Types::SOME); \
+        DECLARE_STRING(str##__tmp, tv->Sel(0)); \
+        s = str##__tmp->ExportC();      \
+    }
+        
+        
+        
+DEFINE3(LibGlade_load) {
+    DECLARE_OPT_CSTRING(domain, x0);
+    DECLARE_CSTRING(filename, x1);
+    DECLARE_OPT_CSTRING(root, x2);
+
+    GladeXML *glade = glade_xml_new_with_domain (filename, root, domain);
+    if (glade == 0) {
+        RAISE_CORE_ERROR("Glade: load failure");
+    }
+
+    RETURN(OBJECT_TO_WORD(glade, TYPE_G_OBJECT, GLADE_TYPE_XML));
+} END
+
+
+DEFINE2(LibGlade_getWidget) {
+    DECLARE_OBJECT_OF_TYPE(g, x0, GLADE_TYPE_XML);
+    DECLARE_CSTRING(name, x1);
+    
+    GladeXML *glade = (GladeXML*)g;
+    RETURN(OBJECT_TO_WORD(glade_xml_get_widget (glade, name)));
+} END
+
+
+
+static void connect_fun (const gchar *handler, GObject *ob, const gchar *sig_name,
+        const gchar *signal_data, GObject *con_ob, gboolean after, gpointer data) {
+    word *res = (word*)data;
+    res[0] = NativeCore_SignalConnect (ob, (char*)sig_name, after == TRUE);
+    res[1] = OBJECT_TO_WORD(ob, TYPE_G_OBJECT);
+}
+
+
+DEFINE2(LibGlade_connect) {
+    DECLARE_OBJECT_OF_TYPE(g, x0, GLADE_TYPE_XML);
+    DECLARE_CSTRING(name, x1);
+    word res[2];
+    glade_xml_signal_connect_full ((GladeXML*)g, name, connect_fun, res);
+    RETURN2(res[0], res[1]);
+} END
+
+word NativeGlade_CreateComponent () {
+    Record *record = Record::New (3);
+    
+    INIT_STRUCTURE(record, "NativeLibs.NativeGlade", "load", LibGlade_load, 3);
+    INIT_STRUCTURE(record, "NativeLibs.NativeGlade", "getWidget", LibGlade_getWidget, 2);
+    INIT_STRUCTURE_N(record, "NativeLibs.NativeGlade", "connect", LibGlade_connect, 2, 2);
+    return record->ToWord ();
+}
+
--- lib/gtk/seam/NativeLibs.cc
+++ lib/gtk/seam/NativeLibs.cc
@@ -6,8 +6,9 @@ word NativeAtk_CreateComponent();
 word NativeGdk_CreateComponent();
 word NativeGtk_CreateComponent();
 word NativeCanvas_CreateComponent();
+word NativeGlade_CreateComponent ();
 word InitComponent () {
-    Record *record = Record::New (7);
+    Record *record = Record::New (8);
     record->Init ("NativeCore$", NativeCore_CreateComponent());
     record->Init ("NativeGLib$", NativeGLib_CreateComponent());
     record->Init ("NativePango$", NativePango_CreateComponent());
@@ -15,5 +16,6 @@ word InitComponent () {
     record->Init ("NativeGdk$", NativeGdk_CreateComponent());
     record->Init ("NativeGtk$", NativeGtk_CreateComponent());
     record->Init ("NativeCanvas$", NativeCanvas_CreateComponent());
+    record->Init ("NativeGlade$", NativeGlade_CreateComponent());
     RETURN_STRUCTURE("NativeLibs$", record);
 }
