60f493223aa50615de1636cbe03dc66bb1d184f5
cmueller <none@none>
2006-05-09 09:38:59 +0100

    adjusted links

--- doc/manual/cptutorial/aliceTutorial.pl
+++ doc/manual/cptutorial/aliceTutorial.pl
@@ -9,12 +9,13 @@ $STYLESHEET = "style.css";
 
 sub top_navigation_panel {
     '<DIV class=margin><H1>alice<BR>constraint<BR>tutorial.<BR><BR><BR></H1>' .
-    '<P class="menu"><A href="index.html">Contents</A><BR>'.
-    '<A href="tour.html">Tour</A><BR>'.
-    '<A href="language.html">Language</A><BR>'.
-    '<A href="library/index.html">Library</A><BR>'.
-    '<A href="tools.html">Tools</A><BR>'.
-    '<A href="sitemap.html">Index</A><BR>'.
+    '<P class="menu"><A href="../index.html">Contents</A><BR>'.
+    '<A href="../tour.html">Tour</A><BR>'.
+    '<A href="../language.html">Language</A><BR>'.
+    '<A href="../library/index.html">Library</A><BR>'.
+    '<A href="index.html">CP Tutorial</A><BR>'.
+    '<A href="../tools.html">Tools</A><BR>'.
+    '<A href="../sitemap.html">Index</A><BR>'.
     '</P><A href="http://www.ps.uni-sb.de/alice/">'.
     '<IMG src="logo-small.gif" border=0 alt="Alice Project"></A></DIV>' .
     &navigation_panel
