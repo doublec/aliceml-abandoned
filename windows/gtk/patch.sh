15a6acec787ed5db37c5d6516d9f12e138c71530
gareth@gareth-71411bfa <gareth@gareth-71411bfa>
2011-07-05 19:51:17 +0100

    update the windows gtk binaries to version 2.22

---
+++ windows/gtk/patch.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PWD=$(pwd)
+
+echo "ersetze alle Dateien in denen der String \
+/home/pslab/Projects/WinGtk-2.2 vorkommt durch $(PWD)"
+
+SED_EXPRESSION='s#prefix=.*#prefix='$PWD'#'
+
+for i in `find $PWD/lib/pkgconfig -name '*.pc*'`; do
+	sed -r -n -e $SED_EXPRESSION -e "w $i.tmp" $i
+	echo "$i"
+	mv "$i.tmp" "$i"
+done
+
+# these need to be executable, but they arent by default
+find . -name '*.exe' | xargs chmod +x
+find . -name '*.dll' | xargs chmod +x
+
+
