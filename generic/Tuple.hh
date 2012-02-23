cf7daa13f52cfdd2e0e1c75c70418ed353c66136
garethdanielsmith@gmail.com <garethdanielsmith@gmail.com>
2012-02-23 22:00:47 +0000

    add convenience constructors for small tuples

--- generic/Tuple.hh
+++ generic/Tuple.hh
@@ -23,16 +23,40 @@ class SeamDll Tuple: private Block {
 public:
   using Block::ToWord;
 
-  // Tuple Constructor
+  // Tuple Constructors
+  
   static Tuple *New(u_int n) {
     return static_cast<Tuple *>(Store::AllocBlock(TUPLE_LABEL, n));
   }
+  
+  static Tuple *Single(word a) {
+    Tuple *t = New(1);
+    t->Init(0, a);
+    return t;
+  }
+  
+  static Tuple *Pair(word a, word b) {
+    Tuple *t = New(2);
+    t->Init(0, a);
+    t->Init(1, b);
+    return t;
+  }
+  
+  static Tuple *Triple(word a, word b, word c) {
+    Tuple *t = New(3);
+    t->Init(0, a);
+    t->Init(1, b);
+    t->Init(2, c);
+    return t;
+  }
+  
   // Tuple Untagging
   static Tuple *FromWord(word x) {
     Block *b = Store::WordToBlock(x);
     Assert(b == INVALID_POINTER || b->GetLabel() == TUPLE_LABEL);
     return static_cast<Tuple *>(b);
   }
+  
   static Tuple *FromWordDirect(word x) {
     Block *b = Store::DirectWordToBlock(x);
     Assert(b->GetLabel() == TUPLE_LABEL);
@@ -43,9 +67,11 @@ public:
   void AssertWidth(u_int n) {
     Assert(Store::SizeToBlockSize(n) == GetSize()); n = n;
   }
+  
   void Init(u_int index, word value) {
     InitArg(index, value);
   }
+  
   word Sel(u_int index) {
     return GetArg(index);
   }
